b5c3d69e462503f238f44aa78ac0ef7b0d63e408
Felipe <felipemoreira1@gmail.com>
2022-05-02 12:52:28 -0300

    Collatz Conjecture

---
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -0,0 +1,19 @@
+export const steps = (number) => {
+  if (number < 1) {
+    throw new Error('Only positive numbers are allowed');
+  }
+
+  let n = number;
+  let steps = 0;
+  while(n > 1) {
+    if (n % 2 === 0) {
+      n /= 2;
+    } else {
+      n = 3 * n + 1;
+    }
+
+    steps++;
+  }
+
+  return steps;
+};

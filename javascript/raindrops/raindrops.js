8558ad27092c8ba1b99f3ca22a57c6dfd8231cdb
Felipe <felipemoreira1@gmail.com>
2022-05-26 09:05:11 -0300

    Raindrops

---
+++ javascript/raindrops/raindrops.js
@@ -0,0 +1,21 @@
+export const convert = (number) => {
+  let result = "";
+
+  if (number % 3 === 0) {
+    result += "Pling";
+  } 
+
+  if (number % 5 === 0) {
+    result += "Plang";
+  }
+
+  if (number % 7 === 0) {
+    result += "Plong";
+  }
+
+  if (result === "") {
+    result += number;
+  }
+
+  return result;
+};

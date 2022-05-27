bd81d7510b3103459d7ac49212600835f1cb1c7e
Felipe <felipemoreira1@gmail.com>
2022-05-27 10:03:59 -0300

    Exercism Java: Say (until hundreds)

---
+++ java/say/src/main/java/Say.java
@@ -0,0 +1,114 @@
+
+public class Say {
+
+    public String say(long number) {
+        String s = "";
+        if (number == 0) {
+            return "zero";
+        }
+
+        int u = (int) (number % 10);
+        s += units(u);
+
+        int t = (int) (number % 100);
+
+        if (t > 10 && t < 20) {
+            s = teens(t);
+        } else if (t >= 20) {
+            t -= u;
+            if (u == 0) {
+                s = tens(t);
+            } else {
+                s = tens(t) + "-" + s;
+            }
+        }
+
+        int h = (int) (number % 1000);
+        h /= 100;
+        if (h > 0) {
+            s = units(h) + " hundred " + s;
+        }
+
+        return s.trim();
+    }
+
+    private String units(int number) {
+        switch (number) {
+            case 1:
+                return "one";
+            case 2:
+                return "two";
+            case 3:
+                return "three";
+            case 4:
+                return "four";
+            case 5:
+                return "five";
+            case 6:
+                return "six";
+            case 7:
+                return "seven";
+            case 8:
+                return "eight";
+            case 9:
+                return "nine";
+            default:
+                return "";
+                
+        }
+    }
+
+    private String teens(int number) {
+        
+        switch (number) {
+            case 11:
+                return "eleven";
+            case 12:
+                return "twelve";
+            case 13:
+                return "thirteen";
+            case 14:
+                return "fourteen";
+            case 15:
+                return "fifteen";
+            case 16:
+                return "sixteen";
+            case 17:
+                return "seventeen";
+            case 18:
+                return "eighteen";
+            case 19:
+                return "nineteen";
+                
+        }
+
+        return "";
+    }
+
+    private String tens(int number) {
+        
+        switch (number) {
+            case 10:
+                return "ten";
+            case 20:
+                return "twenty";
+            case 30:
+                return "thirty";
+            case 40:
+                return "forty";
+            case 50:
+                return "fifty";
+            case 60:
+                return "sixty";
+            case 70:
+                return "seventy";
+            case 80:
+                return "eighty";
+            case 90:
+                return "ninety";
+            default:
+                return "";
+                
+        }
+    }
+}

e6bae449d2d696e0a0b2d611b533ace109a11159
Felipe <felipemoreira1@gmail.com>
2022-05-29 10:40:48 -0300

    Simplified it a little bit

--- javascript/clock/clock.js
+++ javascript/clock/clock.js
@@ -23,30 +23,15 @@ export class Clock {
 
   plus(minutes) {
     this.minutes += minutes;
-    return this;
+    return new Clock(0, this.minutes);
   }
 
   minus(minutes) {
     this.minutes -= minutes;
-    let dayInMinutes = 24 * 60;
-    while(this.minutes < 0) {
-      this.minutes += dayInMinutes;
-    }
-    return this;
+    return new Clock(0, this.minutes);;
   }
 
   equals(other) {
-    let thisH = (Math.floor(this.minutes / 60)) % 24;
-    let thisM = this.minutes % 60;
-    thisH = thisH >= 10 ? thisH : '0' + thisH;
-    thisM = thisM >= 10 ? thisM: '0' + thisM;
-
-    let otherH = (Math.floor(other.minutes / 60)) % 24;
-    let otherM = other.minutes % 60;
-    otherH = otherH >= 10 ? otherH : '0' + otherH;
-    otherM = otherM >= 10 ? otherM: '0' + otherM;
-
-
-    return (thisH === otherH) && (thisM === otherM);
+    return this.toString() === other.toString();
   }
 }

c1720822a9711c6b6ac71b9d6bfa4edae51d298c
Felipe <felipemoreira1@gmail.com>
2022-05-30 10:46:35 -0300

    feat: Wordy - Everything working: Ugly solution

--- java/wordy/src/main/java/WordProblemSolver.java
+++ java/wordy/src/main/java/WordProblemSolver.java
@@ -1,45 +1,40 @@
-import java.security.AllPermission;
-
 public class WordProblemSolver {
 
     public int solve(String string) {
 
-        String initialPattern = "What is.*";
+        try{
+            int result = 0;
 
-        if (!string.matches(initialPattern)) {
+            String operation = string.replace("What is ", "");
+            String[] tokens = operation.split("\\s+(by)*\\s*");
+    
+            if (tokens.length == 1) {            
+                String number = tokens[0].replaceAll("[^\\d]", "");
+                result = Integer.parseInt(number);
+            } else if (tokens.length == 3) {
+                int firstNumber = Integer.parseInt(tokens[0]);
+                String oper = tokens[1];
+                int secondNumber = Integer.parseInt(tokens[2].replaceAll("[^-\\d]", ""));
+    
+                result = applyOperation(firstNumber, secondNumber, oper);
+            } else if (tokens.length == 5) {
+                int firstNumber = Integer.parseInt(tokens[0]);
+                String firstOper = tokens[1];
+                int secondNumber = Integer.parseInt(tokens[2]);
+                String secondOper = tokens[3];
+                int thirdNumber = Integer.parseInt(tokens[4].replaceAll("[^-\\d]", ""));
+    
+                result = applyOperation(firstNumber, secondNumber, firstOper);
+                result = applyOperation(result, thirdNumber, secondOper);
+            } else {
+                throw new Exception();
+            }
+    
+            return result;
+        } catch (Exception e) {
             throw new IllegalArgumentException("I'm sorry, I don't understand the question!");
-        }
-
-        int result = 0;
-
-        String operation = string.replace("What is ", "");
-        String[] tokens = operation.split("\\s+(by)*\\s*");
-
-        if (tokens.length == 1) {            
-            String number = tokens[0].replaceAll("[^\\d]", "");
-            result = Integer.parseInt(number);
-        }
-
-        if (tokens.length == 3) {
-            int firstNumber = Integer.parseInt(tokens[0]);
-            String oper = tokens[1];
-            int secondNumber = Integer.parseInt(tokens[2].replaceAll("[^-\\d]", ""));
-
-            result = applyOperation(firstNumber, secondNumber, oper);
-        }
-
-        if (tokens.length == 5) {
-            int firstNumber = Integer.parseInt(tokens[0]);
-            String firstOper = tokens[1];
-            int secondNumber = Integer.parseInt(tokens[2]);
-            String secondOper = tokens[3];
-            int thirdNumber = Integer.parseInt(tokens[4].replaceAll("[^-\\d]", ""));
-
-            result = applyOperation(firstNumber, secondNumber, firstOper);
-            result = applyOperation(result, thirdNumber, secondOper);
-        }
-
-        return result;
+        }        
+        
     }
 
     private int applyOperation(int firstNumber, int secondNumber, String operation){

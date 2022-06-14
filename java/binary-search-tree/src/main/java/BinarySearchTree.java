68ce73814a53e0baf4ba22b82a2ab6f7a831d6f3
Felipe <felipemoreira1@gmail.com>
2022-06-13 21:48:28 -0300

    Binary Search Tree

--- java/binary-search-tree/src/main/java/BinarySearchTree.java
+++ java/binary-search-tree/src/main/java/BinarySearchTree.java
@@ -2,7 +2,6 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
-import java.util.stream.Collectors;
 
 class BinarySearchTree<T extends Comparable<T>> {
 

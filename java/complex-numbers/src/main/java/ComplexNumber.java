356c5cb98fdd392904657dfdc2d957b4bbcdf5a8
Felipe <felipemoreira1@gmail.com>
2022-06-14 09:04:07 -0300

    Complex Numbers: feat: Criação do número

---
+++ java/complex-numbers/src/main/java/ComplexNumber.java
@@ -0,0 +1,46 @@
+public class ComplexNumber {
+
+    private final double real;
+    private final double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return this.real;
+    }
+
+    public double getImag() {
+        return this.imaginary;
+    }
+
+    public ComplexNumber add(ComplexNumber other) {
+        return null; //TODO
+    }
+
+    public ComplexNumber minus(ComplexNumber other) {
+        return null; //TODO
+    }
+
+    public ComplexNumber times(ComplexNumber other) {
+        return null; //TODO
+    }
+
+    public ComplexNumber div(ComplexNumber other) {
+        return null; //TODO
+    }
+
+    public double abs() {
+        return -1; //TODO
+    }
+
+    public ComplexNumber conjugate() {
+        return null; //TODO
+    }
+
+    public ComplexNumber exponentialOf() {
+        return null; //TODO
+    }
+}

4c8600ca8c13d13410dac4399fe6b3052ba00a21
Felipe <felipemoreira1@gmail.com>
2022-06-16 13:05:27 -0300

    Custom Set - Implementação inicial
    Todos os métodos implementados e 27/38 testes passando.

---
+++ java/custom-set/src/main/java/CustomSet.java
@@ -0,0 +1,105 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomSet<T> {
+
+    private int size;
+    private final List<T> setItems;
+    public CustomSet() {
+        this(new ArrayList<>());
+    }
+
+    public CustomSet(List<T> items) {
+        this.size = 0;
+        this.setItems = new ArrayList<>();
+
+        items.forEach(item -> {
+            if (!this.setItems.contains(item)) {
+                this.setItems.add(item);
+                this.size++;
+            }
+        });
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public boolean contains(T item) {
+        return this.setItems.contains(item);
+    }
+
+    public boolean add(T item) {
+        if (!this.setItems.contains(item)) {
+            this.setItems.add(item);
+            this.size++;
+            System.out.println(this.toString());
+            return true;
+        }
+
+        return false;
+    }
+
+    public List<T> getValues() {
+        return new ArrayList<>(this.setItems);
+    }
+
+    public boolean isSubset(CustomSet<T> other) {
+
+        if (other.isEmpty()) {
+            return true;
+        }
+
+        boolean containsAll = true;
+        for (T item : other.getValues()){
+            containsAll = containsAll && this.setItems.contains(item);
+        }
+
+        return containsAll;
+    }
+
+    public CustomSet<T>  getIntersection(CustomSet<T> other) {
+        List<T> allItems = new ArrayList<>(this.getValues());
+        allItems.addAll(other.getValues());
+
+        CustomSet<T> intersection = new CustomSet<>();
+        for (T item : allItems) {
+            if (this.contains(item) && other.contains(item)) {
+                intersection.add(item);
+            }
+        }
+
+        return intersection;
+    }
+
+    public boolean isDisjoint(CustomSet<T> other) {
+        return this.getIntersection(other).isEmpty();
+    }
+
+    public CustomSet<T> getDifference(CustomSet<T> other) {
+        CustomSet<T> difference = new CustomSet<>();
+
+        for (T item : this.getValues()) {
+            if (!other.contains(item)) {
+                difference.add(item);
+            }
+        }
+
+        return difference;
+    }
+
+    public CustomSet<T> getUnion(CustomSet<T> other) {
+        List<T> allItems = new ArrayList<>(this.getValues());
+        allItems.addAll(other.getValues());
+
+        return new CustomSet<>(allItems);
+    }
+
+    @Override
+    public String toString() {
+        return "CustomSet{" +
+                "size=" + size +
+                ", setItems=" + setItems +
+                '}';
+    }
+}

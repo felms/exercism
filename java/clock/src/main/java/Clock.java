5b605b8ae17648b2c2571b2e9b862b372a2ae1b1
Felipe <felipemoreira1@gmail.com>
2022-06-15 22:34:56 -0300

    Clock - feat: Criação com inputs negativos

--- java/clock/src/main/java/Clock.java
+++ java/clock/src/main/java/Clock.java
@@ -3,18 +3,24 @@ public class Clock {
     private int minutes;
 
     public Clock(int hours, int minutes) {
-        int h = hours;
-        int m = minutes;
 
+        int h = hours >= 0 ? hours % 24 : 24 + hours % 24;
+
+        int m = 0;
         if (minutes >= 60) {
-            m = minutes % 60;
             h += minutes / 60;
+            h %= 24;
+            m = minutes % 60;
+        } else if (minutes >= 0) {
+            m = minutes;
+        } else {
+            m = -Math.abs(minutes) % 60;
+            h += -(Math.abs(minutes) % 1440) / 60;
+            h = h < 0 ? 24 + h : h;
         }
 
-        h = h >= 24 ? h % 24 : h;
-
-        this.minutes = m;
-        this.minutes += h * 60;
+        this.minutes = h * 60;
+        this.minutes += m;
     }
 
     public void add(int minutes) {

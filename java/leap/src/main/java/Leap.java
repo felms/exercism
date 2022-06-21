b016f2dd3f5508235a9010f8aed5137e67345139
Felipe <felipemoreira1@gmail.com>
2022-06-21 11:36:52 -0300

    Leap

---
+++ java/leap/src/main/java/Leap.java
@@ -0,0 +1,16 @@
+class Leap {
+
+    boolean isLeapYear(int year) {
+        
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else if (year % 4 == 0) {
+            return true;
+        }
+        
+        return false;
+    }
+
+}

1bcbece3c91d6dbfecdc3320b582e521ec03ee10
Felipe <felipemoreira1@gmail.com>
2022-06-22 20:13:10 -0300

    Yacht - feat: Full House

--- javascript/yacht/yacht.js
+++ javascript/yacht/yacht.js
@@ -27,9 +27,9 @@ export const score = (dice, category) => {
          case 'sixes':
             result += 6 * count[6];
             break;
-//         case 'ones':
-//            result += 1 * count[1];
-//            break;
+         case 'full house':
+            result += calcFullHouse(count); 
+            break;
 //         case 'ones':
 //            result += 1 * count[1];
 //            break;
@@ -39,4 +39,25 @@ export const score = (dice, category) => {
     return result;
 };
 
+const calcFullHouse = (dice) => {
 
+    let result = 0;
+    let gotTrio = false;
+    let gotPair = false;
+
+    for (let i = 0; i < dice.length; i++) {
+        if (dice[i] === 3) {
+            result += 3 * i;
+            gotTrio = true;
+        } else if (dice[i] === 2) {
+            result += 2 * i;
+            gotPair = true;
+        }
+    }
+
+    if (gotPair && gotTrio) {
+        return result;
+    }
+
+    return 0;
+}

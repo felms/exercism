e87be35d4a204d4521cd02e7ee2276991f672a2b
Felipe <felipemoreira1@gmail.com>
2022-07-16 15:38:36 -0300

    Forth - feat: Adição, subtração, multiplicação e divisão

--- javascript/forth/forth.js
+++ javascript/forth/forth.js
@@ -15,6 +15,36 @@ export class Forth {
 
         if (/\d+/.test(expression)) {
             this._stack.push(parseInt(expression));
+        } else if (/[-+*/]/.test(expression)) {
+            this.arithmeticOperation(expression);
         }
     }
+
+    arithmeticOperation(operation) {
+
+        if(this._stack.length < 2) {
+            throw new Error('Stack empty');
+        }
+
+        let a = this._stack.pop();
+        let b = this._stack.pop();
+        switch(operation) {
+            case '+':
+                this._stack.push(b + a);
+                break;
+            case '-':
+                this._stack.push(b - a);
+                break;
+            case '*':
+                this._stack.push(b * a);
+                break;
+            case '/':
+                if (a == 0) {
+                    throw new Error('Division by zero');
+                }
+                this._stack.push(Math.trunc(b / a));
+                break;
+        }
+    }
+
 }

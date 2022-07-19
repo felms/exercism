c8bd4a6a4e257089c5fd9cf7c41937fa373fe456
Felipe <felipemoreira1@gmail.com>
2022-07-19 16:33:53 +0000

    List Ops - feat: 'append' e 'concat'

---
+++ javascript/list-ops/list-ops.js
@@ -0,0 +1,54 @@
+export class List {
+    constructor(items = []) {
+        this._items = items;
+    }
+
+    append(otherList) {
+        let arr = [...this._items];
+
+        for (let item of otherList.values) {
+            arr.push(item);
+        }
+        return new List(arr);
+    }
+
+    get values() {
+        return this._items;
+    }
+
+    concat(otherList) {
+        let arr = [...this._items];
+        let newArr = [];
+
+        for (let item of otherList.values) {
+                arr.push(...item.values);
+        }
+        return new List(arr);
+
+    }
+
+    filter() {
+        throw new Error('Remove this statement and implement this function');
+    }
+
+    map() {
+        throw new Error('Remove this statement and implement this function');
+    }
+
+    length() {
+        throw new Error('Remove this statement and implement this function');
+    }
+
+    foldl() {
+        throw new Error('Remove this statement and implement this function');
+    }
+
+    foldr() {
+        throw new Error('Remove this statement and implement this function');
+    }
+
+    reverse() {
+        throw new Error('Remove this statement and implement this function');
+    }
+
+}

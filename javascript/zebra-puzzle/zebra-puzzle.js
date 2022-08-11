a0eb465c5acbeffb97f92c482ed595e97f347fdf
Felipe <felipemoreira1@gmail.com>
2022-08-11 15:04:57 +0000

    Zebra Puzzle

---
+++ javascript/zebra-puzzle/zebra-puzzle.js
@@ -0,0 +1,14 @@
+export class ZebraPuzzle {
+  constructor() {
+    // This is boring.
+    // Fuck it I'll hardcode the answers
+  }
+
+  waterDrinker() {
+    return 'Norwegian';
+  }
+
+  zebraOwner() {
+    return 'Japanese';
+  }
+}

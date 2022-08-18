124e91c294c450fc801d7eaf55b6f76ae2f96356
Felipe <felipemoreira1@gmail.com>
2022-08-18 17:19:06 +0000

    OCR Numbers - refactor
    Percebi que estava criando nove constantes para calcular 10 números.
    Achei perda de tempo e resolvi criar uma constante para cada número
    e jogar em um switch de uam vez.

--- javascript/ocr-numbers/ocr-numbers.js
+++ javascript/ocr-numbers/ocr-numbers.js
@@ -1,65 +1,38 @@
-const UPPER_EMPTY = '   ';
-const UPPER_UNDERSCORE = ' _ ';
-
-const MIDDLE_PIPES = '| |';
-const MIDDLE_R_PIPE = '  |';
-const MIDDLE_B_RIGHT = ' _|';
-const MIDDLE_CUP = '|_|';
-const MIDDLE_L_BOTTOM = '|_ ';
-
-const LOWER_CUP = '|_|';
-const LOWER_R_PIPE = '  |';
-const LOWER_L_BOTTOM = '|_ ';
-const LOWER_B_RIGHT = ' _|';
+const ZERO = '_ | ||_|';
+const ONE = '|  |';
+const TWO = '_  _||_';
+const THREE = '_  _| _|';
+const FOUR = '|_|  |';
+const FIVE = '_ |_  _|';
+const SIX = '_ |_ |_|';
+const SEVEN = '_   |  |';
+const EIGHT = '_ |_||_|';
+const NINE = '_ |_| _|';
 
 export const convert = (grid) => {
-  let matrix = grid.split('\n');
+  let number = grid.trim().split('\n').join('');
 
-  // Primeira linha só é vazia nos números 1 e 4
-  if (matrix[0] === UPPER_EMPTY) { 
-    if (matrix[1] === MIDDLE_CUP) {  
-      return '4';
-    }
-
-    return '1';
-  }
-  
-  // Todos os outros casos terão o mesmo 
-  // padrão na primeira linha.
-  // A diferença vem nas próximas
-
-  if (matrix[1] === MIDDLE_R_PIPE) {
-    return '7';
-  }
-
-  if (matrix[1] === MIDDLE_PIPES) {
-    return '0';
-  }
-
-  // 2 ou 3
-  if (matrix[1] === MIDDLE_B_RIGHT) {
-    if (matrix[2] === LOWER_L_BOTTOM) {
+  switch(number) {
+    case ZERO:
+      return '0';
+    case ONE:
+      return '1';
+    case TWO:
       return '2';
-    }
-
-    return '3';
-  }
-
-  // 5 ou 6
-  if (matrix[1] === MIDDLE_L_BOTTOM) {
-    if (matrix[2] === LOWER_B_RIGHT) {
+    case THREE:
+      return '3';
+    case FOUR:
+      return '4';
+    case FIVE:
       return '5';
-    }
-
-    return '6';
-  }
-
-  // 8 ou 9
-  if (matrix[1] === MIDDLE_CUP) {
-    if (matrix[2] === LOWER_CUP) {
+    case SIX:
+      return '6';
+    case SEVEN:
+      return '7';
+    case EIGHT:
       return '8';
-    }
-
-    return '9';
+    case NINE:
+      return '9';
   }
+
 };

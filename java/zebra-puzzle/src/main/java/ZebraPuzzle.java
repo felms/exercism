77a087e8e1a7359a314173a06db1f4c5ef3f98be
felipe <felipemoreira1@gmail.com>
2022-08-20 11:17:00 -0300

    Zebra Puzzle
    Esse é chato demais. Tava sem saco e 'hardcoded' as respostas.

---
+++ java/zebra-puzzle/src/main/java/ZebraPuzzle.java
@@ -0,0 +1,15 @@
+public class ZebraPuzzle {
+
+    public ZebraPuzzle() {
+        // This is boring.
+        // Fuck it I'll hardcode the answers
+    }
+
+    public String getWaterDrinker() {
+        return "Norwegian";
+    }
+
+    public String getZebraOwner() {
+        return "Japanese";
+    }
+}

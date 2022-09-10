bf35234c89e6c8583ab97fea3f8a15d580348142
felipe <felipemoreira1@gmail.com>
2022-09-10 19:32:43 -0300

    Pythagorean Triplet - refactor

--- java/pythagorean-triplet/src/main/java/PythagoreanTriplet.java
+++ java/pythagorean-triplet/src/main/java/PythagoreanTriplet.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class PythagoreanTriplet {
@@ -37,4 +39,44 @@ public class PythagoreanTriplet {
                 ", c=" + c +
                 '}';
     }
+
+
+    static class PythagoreanTripletBuilder {
+
+        private int maxFactorValue;
+        private int sumTo;
+
+        public PythagoreanTripletBuilder() {
+            this.maxFactorValue = Integer.MAX_VALUE;
+            this.sumTo = Integer.MAX_VALUE;
+        }
+
+        public PythagoreanTripletBuilder thatSumTo(int i) {
+            this.sumTo = i;
+            return this;
+        }
+
+        public List<PythagoreanTriplet> build() {
+
+            List<PythagoreanTriplet> triplets = new ArrayList<>();
+
+            for (int a = 1; a < this.maxFactorValue && a < this.sumTo; a++) {
+                for (int b = a + 1; b < this.maxFactorValue && b < this.sumTo; b++) {
+                    int c = (int) Math.sqrt(a * a + b * b);
+                    if (c <= this.maxFactorValue && a * a + b * b == c * c
+                            && a + b + c == this.sumTo) {
+                        triplets.add(new PythagoreanTriplet(a, b, c));
+                    }
+                }
+            }
+
+            return triplets;
+        }
+
+        public PythagoreanTripletBuilder withFactorsLessThanOrEqualTo(int i) {
+            this.maxFactorValue = i;
+            return this;
+        }
+    }
+
 }

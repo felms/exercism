9b0ee2274c3b556d259c27513a0fa6d1182aae2f
felipe <felipemoreira1@gmail.com>
2022-09-28 21:45:53 -0300

    Passando em 08/09 testes

--- java/hangman/src/main/java/Hangman.java
+++ java/hangman/src/main/java/Hangman.java
@@ -1,4 +1,5 @@
 import io.reactivex.Observable;
+
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
@@ -21,24 +22,73 @@ public class Hangman {
         Set<String> missesSet = new HashSet<>();
         List<Part> parts = new ArrayList<>();
         Queue<Part> allParts = new ArrayDeque<>(List.of(Part.values()));
-        List<Status> currentStatus = new ArrayList<>();
-        currentStatus.add(Status.PLAYING);
+        Status[] currentStatus = {Status.PLAYING};
+        boolean[] hasError = {false};
+        String[] errorMessage = {""};
+
+        List<Output> outputs = new ArrayList<>();
+
+        secret.subscribe(s -> {
+            outputs.add(new Output(
+                    String.join("", secretWord),
+                    String.join("", discoveredLetters),
+                    guessesSet,
+                    missesSet,
+                    parts,
+                    currentStatus[0])
+            );
+        });
 
         guesses.subscribe(
-                new Observer<String>() {
+                new Observer<>() {
                     @Override
                     public void onSubscribe(Disposable d) {
-                          
+
                     }
 
                     @Override
-                    public void onNext(String s) {
+                    public void onNext(String letter) {
+                        if (guessesSet.contains(letter) || missesSet.contains(letter)) {
+                            onError(new IllegalArgumentException("Letter " + letter + " was already played"));
+                        } else {
 
+                            if (secretWord.contains(letter)) {
+
+                                for (int i = 0; i < secretWord.size(); i++) {
+                                    if (secretWord.get(i).equals(letter)) {
+                                        discoveredLetters.set(i, letter);
+                                        guessesSet.add(letter);
+
+                                        if (!discoveredLetters.contains("_")) {
+                                            currentStatus[0] = Status.WIN;
+                                        }
+                                    }
+                                }
+
+                            } else {
+                                missesSet.add(letter);
+                                parts.add(allParts.poll());
+
+                                if (allParts.isEmpty()) {
+                                    currentStatus[0] = Status.LOSS;
+                                }
+                            }
+                        }
+
+                        outputs.add(new Output(
+                                String.join("", secretWord),
+                                String.join("", discoveredLetters),
+                                guessesSet,
+                                missesSet,
+                                parts,
+                                currentStatus[0]
+                        ));
                     }
 
                     @Override
                     public void onError(Throwable e) {
-
+                        hasError[0] = true;
+                        errorMessage[0] = e.getMessage();
                     }
 
                     @Override
@@ -48,13 +98,11 @@ public class Hangman {
                 }
         );
 
-        return Observable.just(new Output(
-                String.join("", secretWord),
-                String.join("", discoveredLetters),
-                guessesSet,
-                missesSet,
-                parts,
-                currentStatus.get(0)
-        ));
+        if (hasError[0]) {
+            return Observable.error(new IllegalArgumentException(errorMessage[0]));
+        }
+
+        return Observable.fromIterable(outputs);
     }
+
 }

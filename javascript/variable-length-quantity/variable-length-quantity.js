337cfd92c5bb8ce2da6704c7ffbc16beea44d816
Felipe <felipemoreira1@gmail.com>
2022-10-07 19:40:54 +0000

    Variable Length Quantity - passando em todos os testes ✓
    Resolvido usando manipulação de strings.
    Quero resolver novamente depois usando apenas manipulação de bits.

--- javascript/variable-length-quantity/variable-length-quantity.js
+++ javascript/variable-length-quantity/variable-length-quantity.js
@@ -10,18 +10,31 @@ export const decode = (numbers) => {
 
   let res = [];
   let sum = [];
-  numbers.forEach(number => {
-    console.log(`number: ${number}`)
-    let strRep = number.toString(2);
-    console.log(`strRep: ${strRep}`);
-    if (strRep.length === 8) {
-      strRep = strRep.substring(1);
+  numbers.forEach((number, index) => {
+
+    // converte o número para a uma string
+    // da representação em binário.
+    // o padStart é para os casos em que o número
+    // é menor que 1 byte
+    let strRep = number.toString(2).padStart(8, '0');
+
+    // cria uma nova string sem o 'most significant bit'
+    // e adiciona à soma
+    let n = strRep.substring(1);
+    sum.push(n.padStart(7, '0'));
+
+    // se o MSB é '0' soma-se os itens 
+    // armazena o número
+    // e zera o array
+    if (strRep.charAt(0) === '0') {
+      res.push(parseInt(sum.join(''), 2));
+      sum = [];
+    } else if (strRep.charAt(0) === '1' && index === numbers.length - 1) {
+      // caso o MSB for '1' no último item de uma sequencia
+      throw new Error('Incomplete sequence');
     }
-    sum.push(strRep.padStart(7, '0'));
-    console.log(`sum: ${sum}`);
   });
 
-  res.push(parseInt(sum.join(''), 2));
   return res;
 
 };
@@ -35,7 +48,7 @@ const encodeNumber = (number) => {
   }
 
   // para numeros > 1 byte
-  
+
   // transoforma o numero para 
   // uma string binario
   let strRep = number.toString(2);
@@ -70,20 +83,3 @@ const encodeNumber = (number) => {
   return res;
 };
 
-const decodeNumber = (number) => {
-
-  // não precisa fazer nada em numeros
-  // de apenas um byte
-  if (number < 128) {
-    return number;
-  }
-
-  // para numeros > 1 byte
-  
-  // transforma o numero para uma string binaria,
-  // retira o 'most significant bit',
-  // converte para int de novo e retorna
-  let strRep = number.toString(2).substring(1);
-
-  return parseInt(strRep, 2);
-};

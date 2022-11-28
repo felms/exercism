ab42bcbe5630e028e024aebd158d658f318fbe35
felipe <felipemoreira1@gmail.com>
2022-11-28 18:42:02 -0300

    Log Level

---
+++ elixir/log-level/lib/log_level.ex
@@ -0,0 +1,26 @@
+defmodule LogLevel do
+  def to_label(level, legacy?) do
+    cond do
+      (level === 0 or level === 5) and legacy? -> :unknown
+      level === 0 -> :trace
+      level === 1 -> :debug
+      level === 2 -> :info
+      level === 3 -> :warning
+      level === 4 -> :error
+      level === 5 -> :fatal
+      true -> :unknown
+    end
+  end
+
+  def alert_recipient(level, legacy?) do
+
+    label = to_label(level, legacy?)
+
+    cond do
+      label === :error or label === :fatal -> :ops
+      label === :unknown and legacy? -> :dev1
+      label === :unknown -> :dev2
+      true -> false
+    end
+  end
+end

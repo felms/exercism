bcbe0fc498f3a6182f65084a4402bdd1adc6c6fa
Felipe <felipemoreira1@gmail.com>
2023-01-04 11:35:12 -0200

    High Score

---
+++ elixir/high-score/lib/high_score.ex
@@ -0,0 +1,28 @@
+defmodule HighScore do
+
+  @initial_score 0
+
+  def new() do
+    Map.new()
+  end
+
+  def add_player(scores, name, score \\ @initial_score) do
+    Map.put(scores, name, score)
+  end
+
+  def remove_player(scores, name) do
+    Map.delete(scores, name)
+  end
+
+  def reset_score(scores, name) do
+    Map.put(scores, name, @initial_score)
+  end
+
+  def update_score(scores, name, score) do
+    Map.update(scores, name, score, fn value -> value + score end)
+  end
+
+  def get_players(scores) do
+    Map.keys(scores)
+  end
+end

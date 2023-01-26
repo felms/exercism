af543fd60277acda837355bb4948d7288d26e279
felipe <felipemoreira1@gmail.com>
2023-01-26 14:12:56 -0300

    Elixir - Sublist

--- elixir/sublist/lib/sublist.ex
+++ elixir/sublist/lib/sublist.ex
@@ -3,32 +3,21 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
-  def compare([], []), do: :equal
+
+  def compare(a, a), do: :equal
   def compare([], _), do: :sublist
   def compare(_, []), do: :superlist
-  def compare(a, a), do: :equal
-  def compare(a, b) when length(a) == length(b), do: :unequal
-  def compare(a, b) when length(a) < length(b) do
 
-    cointained = 
-      Enum.all?(b, fn item -> Enum.member?(a, item) end)
-
-    if cointained do
-      :sublist
-    else
-      :unequal
-    end
-  end
-  def compare(a, b) when length(b) > length(a) do
-
-    cointained = 
-      Enum.all?(a, fn item -> Enum.member?(b, item) end)
-
-    if cointained do
-      :sublist
-    else
-      :unequal
+  def compare(a, b) do
+    cond do
+      sublist?(a, b) -> :sublist
+      sublist?(b, a) -> :superlist
+      true -> :unequal
     end
   end
 
+  defp sublist?(list_a, list_b) do
+    Enum.chunk_every(list_b, length(list_a), 1, :discard)
+    |> Enum.any?(fn item -> item === list_a end)
+  end
 end

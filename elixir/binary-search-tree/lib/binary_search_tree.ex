6658d266d9227b295e0a96cf431cf1f7309b7f59
Felipe <felipemoreira1@gmail.com>
2023-02-08 09:56:38 -0200

    Elixir - Binary Search Tree

---
+++ elixir/binary-search-tree/lib/binary_search_tree.ex
@@ -0,0 +1,33 @@
+defmodule BinarySearchTree do
+  @type bst_node :: %{data: any, left: bst_node | nil, right: bst_node | nil}
+
+  @doc """
+  Create a new Binary Search Tree with root's value as the given 'data'
+  """
+  @spec new(any) :: bst_node
+  def new(data) do
+    %{data: data, left: nil, right: nil}
+  end
+
+  @doc """
+  Creates and inserts a node with its value as 'data' into the tree.
+  """
+  @spec insert(bst_node, any) :: bst_node
+  def insert(tree, data) do
+    cond do
+      tree == nil -> new(data)
+      data <= tree.data -> Map.put(tree, :left, insert(tree.left, data))
+      data > tree.data -> Map.put(tree, :right, insert(tree.right, data))
+    end
+  end
+
+  @doc """
+  Traverses the Binary Search Tree in order and returns a list of each node's data.
+  """
+  @spec in_order(bst_node) :: [any]
+  def in_order(nil), do: []
+
+  def in_order(tree) do
+    in_order(tree.left) ++ [tree.data] ++ in_order(tree.right)
+  end
+end

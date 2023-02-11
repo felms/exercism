f7acffd44897b67cc9e7ce73c863584aac9bfcdc
felipe <felipemoreira1@gmail.com>
2023-02-11 11:22:46 -0300

    Elixir - Yacht (refactor)

--- elixir/yacht/lib/yacht.ex
+++ elixir/yacht/lib/yacht.ex
@@ -17,92 +17,47 @@ defmodule Yacht do
   Calculate the score of 5 dice using the given category's scoring method.
   """
   @spec score(category :: category(), dice :: [integer]) :: integer
-  def score(:ones, dice) do
-    dice
-    |> Enum.count(fn x -> x == 1 end)
-  end
-
-  def score(:twos, dice) do
-    dice
-    |> Enum.count(fn x -> x == 2 end)
-    |> Kernel.*(2)
-  end
-
-  def score(:threes, dice) do
-    dice
-    |> Enum.count(fn x -> x == 3 end)
-    |> Kernel.*(3)
-  end
-
-  def score(:fours, dice) do
-    dice
-    |> Enum.count(fn x -> x == 4 end)
-    |> Kernel.*(4)
-  end
-
-  def score(:fives, dice) do
-    dice
-    |> Enum.count(fn x -> x == 5 end)
-    |> Kernel.*(5)
-  end
-
-  def score(:sixes, dice) do
-    dice
-    |> Enum.count(fn x -> x == 6 end)
-    |> Kernel.*(6)
-  end
+  def score(:ones, dice), do: count(dice, 1)
+  def score(:twos, dice), do: count(dice, 2)
+  def score(:threes, dice), do: count(dice, 3)
+  def score(:fours, dice), do: count(dice, 4)
+  def score(:fives, dice), do: count(dice, 5)
+  def score(:sixes, dice), do: count(dice, 6)
 
   def score(:full_house, dice) do
-    if full_house?(dice) do
-      Enum.sum(dice)
-    else
-      0
+    case Enum.sort(dice) do
+      [x, x, y, y, y] when x != y -> Enum.sum(dice)
+      [x, x, x, y, y] when x != y -> Enum.sum(dice)
+      _ -> 0
     end
   end
 
   def score(:four_of_a_kind, dice) do
-    Enum.filter(dice, fn x -> Enum.count(dice, fn y -> y == x end) >= 4 end)
-    |> Enum.take(4)
-    |> Enum.sum()
+    case Enum.sort(dice) do
+      [_, x, x, x, x] -> 4 * x
+      [x, x, x, x, _] -> 4 * x
+      _ -> 0
+    end
   end
 
   def score(:little_straight, dice) do
-    is_little_straight = Enum.all?(1..5, fn x -> x in dice end)
-
-    if is_little_straight do
-      30
-    else
-      0
+    case Enum.sort(dice) do
+      [1, 2, 3, 4, 5] -> 30
+      _ -> 0
     end
   end
 
   def score(:big_straight, dice) do
-    is_big_straight = Enum.all?(2..6, fn x -> x in dice end)
-
-    if is_big_straight do
-      30
-    else
-      0
+    case Enum.sort(dice) do
+      [2, 3, 4, 5, 6] -> 30
+      _ -> 0
     end
   end
 
   def score(:choice, dice), do: Enum.sum(dice)
 
-  def score(:yacht, dice) do
-    is_yacht =
-      Enum.uniq(dice)
-      |> length()
-      |> Kernel.==(1)
+  def score(:yacht, [x, x, x, x, x]), do: 50
+  def score(:yacht, _), do: 0
 
-    if is_yacht do
-      50
-    else
-      0
-    end
-  end
-
-  defp full_house?(dice) do
-    Enum.any?(dice, fn x -> Enum.count(dice, fn y -> x == y end) == 3 end) and
-      Enum.any?(dice, fn x -> Enum.count(dice, fn y -> x == y end) == 2 end)
-  end
+  defp count(dice, number), do: dice |> Enum.count(fn x -> x == number end) |> Kernel.*(number)
 end

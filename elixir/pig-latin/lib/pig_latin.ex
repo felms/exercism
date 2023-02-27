c8ea056844214e04596b0e8c7f74d0b4519bf9ed
Felipe <felipemoreira1@gmail.com>
2023-02-27 17:10:56 -0300

    Elixir - Pig Latin (solved with Pattern Matching)

--- elixir/pig-latin/lib/pig_latin.ex
+++ elixir/pig-latin/lib/pig_latin.ex
@@ -2,44 +2,40 @@ defmodule PigLatin do
   @doc """
   Given a `phrase`, translate it a word at a time to Pig Latin.
   """
+
+  @vowels ["a", "e", "i", "o", "u"]
+
   @spec translate(phrase :: String.t()) :: String.t()
   def translate(phrase) do
     phrase
     |> String.split(" ", trim: true)
+    |> Enum.map(&String.graphemes/1)
     |> Enum.map(&to_pig_latin/1)
+    |> Enum.map(&Enum.join/1)
     |> Enum.join(" ")
   end
+  
+  defp to_pig_latin([first_letter | _rest] = word) when first_letter in @vowels,
+    do: word ++ ["a", "y"]
 
-  defp to_pig_latin(word) do
-    cond do
-      word =~ ~r/^([aeiou]+|xr|yt).*/ -> rule_01(word)
-      word =~ ~r/^[xy][bcdfghjklmnpqrstvwxz]+.*/ -> rule_01(word)
-      word =~ ~r/^[bcdfghjklmnpqrstvwxz]+y.*/ -> rule_04(word)
-      word =~ ~r/^[bcdfghjklmnpqrstvwxyz]?qu.*/ -> rule_03(word)
-      word =~ ~r/^[bcdfghjklmnpqrstvwxyz]+.*/ -> rule_02(word)
-    end
+  defp to_pig_latin([first_letter, second_letter | _rest] = word)
+       when first_letter in ["x", "y"] and second_letter not in @vowels,
+       do: word ++ ["a", "y"]
+
+  defp to_pig_latin(["q", "u" | rest]) do
+    rest ++ ["q", "u", "a", "y"]
   end
 
-  defp rule_01(word), do: word <> "ay"
-
-  defp rule_02(word) do
-    [start, rest] =
-      Regex.split(~r/^[bcdfghjklmnpqrstvwxyz]+/, word, trim: true, include_captures: true)
-
-    rest <> start <> "ay"
+  defp to_pig_latin([first_letter, "q", "u" | rest]) when first_letter not in @vowels do
+    rest ++ [first_letter] ++ ["q", "u", "a", "y"]
   end
 
-  defp rule_03(word) do
-    [start, rest] =
-      Regex.split(~r/^[bcdfghjklmnpqrstvwxyz]?qu/, word, trim: true, include_captures: true)
+  defp to_pig_latin([first_letter | rest])
+       when first_letter not in @vowels,
+       do: to_pig_latin(rest ++ [first_letter])
 
-    rest <> start <> "ay"
+  defp to_pig_latin([first_letter, "y" | rest]) when first_letter not in @vowels do
+    ["y"] ++ rest ++ [first_letter] ++ ["a", "y"]
   end
 
-  defp rule_04(word) do
-    [start, rest] =
-      Regex.split(~r/^[bcdfghjklmnpqrstvwxz]+/, word, trim: true, include_captures: true)
-
-    rest <> start <> "ay"
-  end
 end

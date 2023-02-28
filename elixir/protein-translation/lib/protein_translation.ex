62b3627f593280e0faa6c176c884ad46d873f422
Felipe <felipemoreira1@gmail.com>
2023-02-28 10:10:01 -0300

    Elixir - Protein Translation

---
+++ elixir/protein-translation/lib/protein_translation.ex
@@ -0,0 +1,68 @@
+defmodule ProteinTranslation do
+  @codons %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {:ok, list(String.t())} | {:error, String.t()}
+  def of_rna(rna) do
+    rna
+    |> String.graphemes()
+    |> Enum.chunk_every(3, 3)
+    |> Enum.map(&Enum.join/1)
+    |> translate([])
+  end
+
+  defp translate([], acc), do: {:ok, acc}
+
+  defp translate([h | t], acc) do
+    case of_codon(h) do
+      {:ok, "STOP"} -> {:ok, acc}
+      {:ok, codon} -> translate(t, acc ++ [codon])
+      {:error, _} -> {:error, "invalid RNA"}
+    end
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {:ok, String.t()} | {:error, String.t()}
+  def of_codon(codon) when is_map_key(@codons, codon), do: {:ok, @codons[codon]}
+  def of_codon(_codon), do: {:error, "invalid codon"}
+end

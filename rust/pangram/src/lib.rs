67dcd589b8c4528dee99dba50734dc7b9ec05d57
felipe <felipemoreira1@gmail.com>
2023-03-10 20:48:16 -0300

    Rust - Pangram: refactor

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,13 +1,6 @@
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    let alphabet = vec!["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-                            "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
     let s = sentence.to_lowercase();
-
-    alphabet.iter()
-        .filter(|&letter| s.contains(letter))
-        .collect::<Vec<_>>()
-        .len() == 26
-    
+    ('a'..='z').all(|letter| s.contains(letter))
 }

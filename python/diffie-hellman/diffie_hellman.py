818d4aea4b6e4e2053fa84e82cdab12a719af42b
Felipe <felipemoreira1@gmail.com>
2023-04-19 09:53:43 -0300

    Python - Diffie-Hellman

---
+++ python/diffie-hellman/diffie_hellman.py
@@ -0,0 +1,12 @@
+import random
+
+def private_key(p):
+    return random.randint(2, p - 1)
+
+
+def public_key(p, g, private):
+    return (g ** private) % p
+
+
+def secret(p, public, private):
+    return (public ** private) % p

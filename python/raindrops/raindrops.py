81e80867e553af7407e8019c02f22b975a5a5921
Felipe <felipemoreira1@gmail.com>
2023-04-20 11:38:58 -0300

    Python - Raindrops

---
+++ python/raindrops/raindrops.py
@@ -0,0 +1,17 @@
+def convert(number):
+
+    res = ""
+
+    if number % 3 == 0:
+        res += "Pling"
+    
+    if number % 5 == 0:
+        res += "Plang"
+
+    if number % 7 == 0:
+        res += "Plong"
+
+    if res == "":
+        res = str(number)
+    
+    return res

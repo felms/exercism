11aaddb7f85a5a8b651e9a3f970f11eba97e65d7
Felipe <felipemoreira1@gmail.com>
2023-07-04 10:33:55 -0300

    C - Armstrong Numbers

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -1,38 +1,17 @@
 #include "armstrong_numbers.h"
 #include <math.h>
 
-int get_digits(int);
-
 bool is_armstrong_number(int candidate)
 {
-    if (candidate == 0)
-    {
-        return true;
-    }
-
-    int n = candidate;
+    int number = candidate;
     int sum = 0;
-    int power = get_digits(candidate);
-    while (n > 0)
-    {
-        int digit = n % 10;
+    int power = (int) log10(number) + 1;
+
+    while (number > 0) {
+        int digit = number % 10;
         sum += pow(digit, power);
-        n /= 10;
+        number /= 10;
     }
 
     return sum == candidate;
-}
-
-int get_digits(int number)
-{
-
-    int n = number;
-    int digits = 0;
-    while (n > 0)
-    {
-        digits++;
-        n /= 10;
-    }
-
-    return digits;
 }

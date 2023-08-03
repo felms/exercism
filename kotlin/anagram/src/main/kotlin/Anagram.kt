b2d8b35d92c1e13c37d92e0facddf9787d87fe38
Felipe <felipemoreira1@gmail.com>
2023-08-03 14:08:25 -0300

    Kotlin - Anagram

--- kotlin/anagram/src/main/kotlin/Anagram.kt
+++ kotlin/anagram/src/main/kotlin/Anagram.kt
@@ -2,16 +2,11 @@ class Anagram (val word : String) {
 
     fun match(anagrams: Collection<String>): Set<String> {
 
-        val sortedWord = this.word.lowercase().split("").sorted().joinToString("")
-        var result = mutableSetOf<String>()
+        val sortedWord = word.lowercase().split("").sorted().joinToString("")
 
-        for (candidate in anagrams) {
-            val sorted = candidate.lowercase().split("").sorted().joinToString("")
-            if ((word.lowercase() != candidate.lowercase()) && sorted == sortedWord)  {
-                result.add(candidate)
-            }
-        }
-
-        return result
+        return anagrams.filter { 
+            it.lowercase().split("").sorted().joinToString("") == sortedWord
+                && it.lowercase() != word.lowercase()
+        }.toSet()
     }
 }

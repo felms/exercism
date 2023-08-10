ece5de40f61014bcb03b642c0d07153d4b9792b5
Felipe <felipemoreira1@gmail.com>
2023-08-10 11:54:38 -0300

    Kotlin - Scrabble Score

---
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -0,0 +1,19 @@
+object ScrabbleScore {
+
+    fun scoreLetter(c: Char): Int {
+        return when(c) {
+            in "AEIOULNRST" -> 1 
+            in "DG"         -> 2 
+            in "BCMP"       -> 3 
+            in "FHVWY"      -> 4
+            'K'             -> 5
+            in "JX"         -> 8
+            in "QZ"         -> 10
+            else            -> 0
+        }
+    }
+
+    fun scoreWord(word: String): Int {
+        return word.uppercase().sumOf{it -> scoreLetter(it)}
+    }
+}

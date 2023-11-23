d4ab88dfdf2060bbbf7379d37a8398657ff44ef4
Felipe <felipemoreira1@gmail.com>
2023-11-23 09:54:44 -0200

    Rust - Matching Brackets (refactor)

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -2,20 +2,13 @@ pub fn brackets_are_balanced(string: &str) -> bool {
     let mut stack = Vec::new();
 
     for c in string.chars() {
-        if c == '(' || c == '[' || c == '{' {
-            stack.push(c);
-        } else if c == ')' || c == ']' || c == '}' {
-            if stack.is_empty() {
-                return false;
-            }
 
-            let &top = stack.last().unwrap();
-
-            if c == ')' && top == '(' || c == ']' && top == '[' || c == '}' && top == '{' {
-                stack.pop();
-            } else {
-                return false;
-            }
+        match c {
+            '(' | '[' | '{' => stack.push(c),
+            ')' => if stack.pop() != Some('(') { return false; },
+            ']' => if stack.pop() != Some('[') { return false; },
+            '}' => if stack.pop() != Some('{') { return false; },
+            _ => (),
         }
     }
 

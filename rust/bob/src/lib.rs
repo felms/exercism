6edb97c2f8c6314beec521cbd117fbc1d5199b30
Felipe <felipemoreira1@gmail.com>
2023-11-24 10:04:04 -0200

    Rust - Bob (v3)

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,24 +1,17 @@
 pub fn reply(message: &str) -> &str {
-
     let m = message.trim();
 
     if m.is_empty() {
         return "Fine. Be that way!";
     }
 
-    let is_question = m.ends_with("?");
+    let is_question = m.ends_with('?');
     let is_shouting = m.chars().any(|c| c.is_ascii_alphabetic()) && m == m.to_ascii_uppercase();
 
-    if is_question { 
-        return if is_shouting {
-            "Calm down, I know what I'm doing!"
-        } else { "Sure." }
+    match (is_question, is_shouting) {
+        (true, true) => "Calm down, I know what I'm doing!",
+        (true, false) => "Sure.",
+        (false, true) => "Whoa, chill out!",
+        _ => "Whatever.",
     }
-
-    if is_shouting {
-        return "Whoa, chill out!";
-    }
-    
-    "Whatever."
 }
-

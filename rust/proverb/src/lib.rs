b095f4f7938a324ed6804258fe007e06ba586c25
Felipe <felipemoreira1@gmail.com>
2023-11-28 13:06:12 -0200

    Rust - Proverb (v2)

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,18 +1,13 @@
-pub fn build_proverb(list: &[&str]) -> String {
+use std::iter::once; //  Creates an iterator that yields an element exactly once.
 
+pub fn build_proverb(list: &[&str]) -> String {
     if list.is_empty() {
         return String::new();
     }
 
-    let mut provb = Vec::new();
-
-    for index in 1..list.len() {
-        provb.push(format!("For want of a {} the {} was lost.", list[index - 1], list[index]));
-    }
-
-    provb.push(format!("And all for the want of a {}.", list[0]));
-
-    provb.join("\n")
+    list.windows(2)
+        .map(|items| format!("For want of a {} the {} was lost.", items[0], items[1]))
+        .chain(once(format!("And all for the want of a {}.", list[0])))
+        .collect::<Vec<String>>()
+        .join("\n")
 }
-
-

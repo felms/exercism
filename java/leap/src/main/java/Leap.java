2160c634952befe58938b0691c996838ae112529
Felipe <felipemoreira1@gmail.com>
2024-01-16 13:06:12 -0200

    Leap

--- java/leap/src/main/java/Leap.java
+++ java/leap/src/main/java/Leap.java
@@ -1,16 +1,9 @@
 class Leap {
 
     boolean isLeapYear(int year) {
-        
-        if (year % 400 == 0) {
-            return true;
-        } else if (year % 100 == 0) {
-            return false;
-        } else if (year % 4 == 0) {
-            return true;
-        }
-        
-        return false;
+
+        return year % 400 == 0
+            || (year % 4 == 0  && year % 100 != 0);
     }
 
 }
--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -4,17 +4,6 @@
 //
 
 export const isLeap = (year) => {
-  if (year % 4 != 0) {
-    return false;
-  }
-
-  if (year % 100 != 0) {
-    return true;
-  }
-
-  if (year % 400 === 0) {
-    return true;
-  }
-
-  return false;
+    return year % 400 === 0
+        || (year % 4 === 0 && year % 100 !== 0);
 };

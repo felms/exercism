bc0ed9576c56ce0cd17e512533a6e96ed583f796
felipe <felipemoreira1@gmail.com>
2024-01-26 17:51:14 -0300

    Python - High Scores

---
+++ python/high-scores/high_scores.py
@@ -0,0 +1,12 @@
+class HighScores:
+    def __init__(self, scores: list[int]):
+        self.scores = scores
+
+    def latest(self) -> int:
+        return self.scores[-1]
+
+    def personal_best(self) -> int:
+        return max(self.scores)
+
+    def personal_top_three(self) -> list[int]:
+        return sorted(self.scores, reverse=True)[:3]

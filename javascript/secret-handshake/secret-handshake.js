02e4b8a8405fbca5b2036261eb8edebd60b0d19f
Felipe <felipemoreira1@gmail.com>
2024-02-09 10:36:57 -0200

    JavaScript - Secret Handshake (refactor)

--- javascript/secret-handshake/secret-handshake.js
+++ javascript/secret-handshake/secret-handshake.js
@@ -1,47 +1,24 @@
 export const commands = (number) => {
-  let list = [];
-  let n = number;
-  n = decToBin(n);
 
-  if(n % 10 != 0) {
-      list.push("wink");
-      n -= 1;
-  }
+    let actions = {
+        1: "wink",
+        2: "double blink",
+        4: "close your eyes",
+        8: "jump",
+    };
 
-  if (n % 100 != 0) {
-      list.push(("double blink"));
-      n -= 10;
-  }
+    let list = [];
 
-  if (n % 1000 != 0) {
-      list.push("close your eyes");
-      n -= 100;
-  }
+    for (const [code, action] of Object.entries(actions)) {
+        if ((number & code) == code) {
+            list.push(action);
+        }
+    }
 
-  if (n % 10000 != 0) {
-      list.push("jump");
-      n -= 1000;
-  }
-
-  if (n % 100000 != 0) {
-      list = list.reverse();
-  }
-
-  return list;
-
+    if((number & 16) == 16) {
+        list = list.reverse();
+    }
+    
+    return list;
 };
 
-const decToBin = (number) => {
-  let n = number;
-  let result = 0;
-  let count = 0;
-  while(n > 0) {
-      let r = n % 2;
-      n -= r;
-      n /= 2;
-      result = (Math.pow(10, count) * r) + result;
-      count++;
-  }
-
-  return result;
-}

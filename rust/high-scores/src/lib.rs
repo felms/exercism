2659f1174c88fc89852ae0cae9b66ec08b79df51
Felipe <felipemoreira1@gmail.com>
2024-02-23 09:13:28 -0300

    Rust - High Scores

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,31 @@
+#[derive(Debug)]
+pub struct HighScores {
+    scores: Vec<u32>,
+}
+
+impl HighScores {
+    pub fn new(scores: &[u32]) -> Self {
+        HighScores {
+            scores: scores.to_vec(),
+        }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        &self.scores[..]
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().cloned()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().cloned()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut s = self.scores.clone();
+        s.sort_by(|a, b| b.cmp(a));
+        s.truncate(3);
+        s
+    }
+}

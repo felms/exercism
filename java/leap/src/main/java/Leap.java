9e6f04961bbf227e29f593f6659be4690c7c2b2b
felipe <felipemoreira1@gmail.com>
2024-03-08 18:22:51 -0300

    Java - Leap

--- java/leap/src/main/java/Leap.java
+++ java/leap/src/main/java/Leap.java
@@ -1,9 +1,7 @@
 class Leap {
 
     boolean isLeapYear(int year) {
-
-        return year % 400 == 0
-            || (year % 4 == 0  && year % 100 != 0);
+        return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
     }
 
 }

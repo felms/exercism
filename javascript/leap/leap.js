b1fc152cc72f300b4924d3fb596f32b3f917d313
felipe <felipemoreira1@gmail.com>
2024-03-09 16:26:44 -0300

    JavaScript - Leap

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -4,6 +4,5 @@
 //
 
 export const isLeap = (year) => {
-    return year % 400 === 0
-        || (year % 4 === 0 && year % 100 !== 0);
+    return year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
 };

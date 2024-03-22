edc63175a87b10eeb18825f1ea37f5a2353a30e3
Felipe <felipemoreira1@gmail.com>
2024-03-22 15:29:32 -0300

    Java - Linked List

--- java/linked-list/src/main/java/DoublyLinkedList.java
+++ java/linked-list/src/main/java/DoublyLinkedList.java
@@ -1,99 +1,74 @@
-/*
+class DoublyLinkedList<T> {
+    private Element<T> head;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    void push(T value) {
 
-Please remove this comment when submitting your solution.
+        if(this.head == null) {
+            this.head = new Element<>(value, null, null);
+            return;
+        } 
 
-*/
+        Element<T> newNode = new Element<>(value, null, null);
+        Element<T> pointer = this.head;
 
-public class DoublyLinkedList<T> {
-
-    Node head;
-    Node tail;
-    int size;
-
-    public DoublyLinkedList(){
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
-    }
-
-    // Insert value at back
-    public void push(T data) {
-        Node n = new Node(data, null, null);
-        
-        if (size == 0) {
-            this.head = n;
-            this.tail = n;
-        } else {
-            n.previous = this.tail;
-            this.tail.next = n;
-            this.tail = n;
+        while(pointer.next != null) {
+            pointer = pointer.next;
         }
-        
-        size++;
+
+        newNode.prev = pointer;
+        pointer.next = newNode;
+
     }
 
-    // Remove value at back
-    public T pop() {
+    T pop() {
 
-        if (size > 0) {
-            T data = (T) this.tail.data;           
-            this.tail = this.tail.previous;           
-
-            size--;
-
+        if (this.head.next == null) {
+            T data = this.head.value;
+            this.head = null;
             return data;
-        }
-        
-        return null;
-    }
+        } 
 
-    // Remove value at front
-    public T shift() {
-        if (size > 0) {
-            T data = (T) this.head.data;
-            this.head.previous = null;
-            this.head = this.head.next;
-            size--;
+        Element<T> current = this.head;
+        Element<T> lastNode = this.head.next;
 
-            return data;
+        while(lastNode.next != null) {
+            current = lastNode;
+            lastNode = lastNode.next;
         }
 
-        return null;
+        current.next = null;
+        return lastNode.value;
     }
 
-    // Insert value at front)
-    public void unshift(T data) {
-        Node n = new Node(data, null, null);
-        
-        if (size == 0) {
-            this.head = n;
-            this.tail = n;
-        } else {
-            n.next = this.head;
-            this.head.previous = n;
-            this.head = n;
+    void unshift(T value) {
+
+        Element<T> newNode = new Element<>(value, null, null);
+
+        if (this.head != null) {
+            this.head.prev = newNode;
+            newNode.next = this.head;
         }
 
-        size++;
+        this.head = newNode;
     }
 
-}
+    T shift() {
 
-class Node<T> {
+        T data = this.head.value;
+        this.head = this.head.next;
 
-    T data;
-    Node previous;
-    Node next;
-
-    Node(T data, Node previous, Node next) {
-        this.data = data;
-        this.previous = previous;
-        this.next = next;
+        return data;
     }
 
+    private static final class Element<T> {
+        private final T value;
+        private Element<T> prev;
+        private Element<T> next;
+
+        Element(T value, Element<T> prev, Element<T> next) {
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
 }

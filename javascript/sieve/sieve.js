9bd44f988411e6ce1e0c57b00acf40182c437af6
Felipe <felipemoreira1@gmail.com>
2024-03-27 09:34:09 -0300

    JavaScript - Sieve

--- javascript/sieve/sieve.js
+++ javascript/sieve/sieve.js
@@ -1,27 +1,16 @@
 export const primes = (limit) => {
-	
-	let sieve = [];		// Array de primos
-	sieve.push(false);	// Não existem primos
-	sieve.push(false);	// menores que '2'
-	
-	for (let i = 2; i <= limit; i++) {
-		sieve.push(true);
-	}
-	
-	for (let i = 0; i <= limit; i++) {
-		if (sieve[i]) {
-			for (let j = 2; j * i <= limit; j++) {
-				sieve[j * i] = false;
-			}
-		}
-	}
-	
-	let primesUpToLimit = [];
-	for (let i = 0; i <= limit; i++) {
-		if (sieve[i]) {
-			primesUpToLimit.push(i);
-		}
-	}	
+    let sieve = Array(limit + 1).fill(true);
 
-	return primesUpToLimit;
+    sieve[0] = false;
+    sieve[1] = false;
+
+    for (let i = 2; i <= limit; i++) {
+        if (sieve[i]) {
+            for (let j = i + i; j <= limit; j += i) {
+                sieve[j] = false;
+            }
+        }
+    }
+
+    return sieve.reduce((acc, prime, index) => prime ? [...acc, index] : acc, []);
 };

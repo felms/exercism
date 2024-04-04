f38ea9723c68c2ba9320287cd259d49b26153888
Felipe <felipemoreira1@gmail.com>
2024-04-04 12:53:37 -0300

    Java - Calculator Conundrum

--- java/calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ java/calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,30 +1,22 @@
 class CalculatorConundrum {
     public String calculate(int operand1, int operand2, String operation) {
 
-        if (operation == null) {
-            throw new IllegalArgumentException("Operation cannot be null");
+        try {
+
+            int res = switch (operation) {
+                case "+" -> operand1 + operand2;
+                case "*" -> operand1 * operand2;
+                case "/" -> operand1 / operand2;
+                case null -> throw new IllegalArgumentException("Operation cannot be null");
+                case "" -> throw new IllegalArgumentException("Operation cannot be empty");
+                default -> throw new IllegalOperationException("Operation '" + operation + "' does not exist");
+            };
+
+            return String.format("%d %s %d = %d", operand1, operation, operand2, res);
+
+        } catch (ArithmeticException ae) {
+            throw new IllegalOperationException("Division by zero is not allowed", ae);
         }
 
-        if (operation.isBlank()) {
-            throw new IllegalArgumentException("Operation cannot be empty");
-        }
-
-        if ("/".equals(operation) && operand2 == 0) {
-            throw new IllegalOperationException("Division by zero is not allowed", new ArithmeticException());
-        }
-
-        return switch (operation) {
-
-            case "+" -> formatResult(operand1, operand2, operation, operand1 + operand2);
-            case "-" -> formatResult(operand1, operand2, operation, operand1 - operand2);
-            case "*" -> formatResult(operand1, operand2, operation, operand1 * operand2);
-            case "/" -> formatResult(operand1, operand2, operation, operand1 / operand2);
-            default -> throw new IllegalOperationException("Operation '" + operation + "' does not exist");
-
-        };
-    }
-
-    private String formatResult(int operand1, int operand2, String operation, int result) {
-        return String.format("%d %s %d = %d", operand1, operation, operand2, result);
     }
 }

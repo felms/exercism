2a64c6b6c59152da5f42fda2dc932ebeddd62c09
Felipe <felipemoreira1@gmail.com>
2024-04-05 13:02:41 -0300

    Java - Wizards and Warriors 2

--- java/wizards-and-warriors-2/src/main/java/GameMaster.java
+++ java/wizards-and-warriors-2/src/main/java/GameMaster.java
@@ -1,22 +1,23 @@
 public class GameMaster {
 
     public String describe(Character character) {
-        return String.format("You're a level %d %s with %d hit points.",
-                character.getLevel(), character.getCharacterClass(), character.getHitPoints());
+        return "You're a level %d %s with %d hit points."
+            .formatted(character.getLevel(), character.getCharacterClass(), character.getHitPoints());
     }
 
     public String describe(Destination destination) {
-        return String.format("You've arrived at %s, which has %d inhabitants.",
-                destination.getName(), destination.getInhabitants());
+        return "You've arrived at %s, which has %d inhabitants."
+            .formatted(destination.getName(), destination.getInhabitants());
     }
 
     public String describe(TravelMethod travelMethod) {
-        return String.format("You're traveling to your destination %s.",
-                travelMethod.equals(TravelMethod.HORSEBACK) ? "on horseback" : "by walking");
+        return "You're traveling to your destination %s."
+            .formatted(travelMethod == TravelMethod.HORSEBACK ? "on horseback"
+                : travelMethod == TravelMethod.WALKING ? "by walking" : "");
     }
 
     public String describe(Character character, Destination destination, TravelMethod travelMethod) {
-        return String.format("%s %s %s", describe(character), describe(travelMethod), describe(destination));
+        return "%s %s %s".formatted(describe(character), describe(travelMethod), describe(destination));
     }
 
     public String describe(Character character, Destination destination) {

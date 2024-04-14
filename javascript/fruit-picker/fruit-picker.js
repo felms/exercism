411a670332527cc49f641fdee7ede646e2dfa15e
felipe <felipemoreira1@gmail.com>
2024-04-14 10:26:34 -0300

    JavaScript - Fruit Picker

--- javascript/fruit-picker/fruit-picker.js
+++ javascript/fruit-picker/fruit-picker.js
@@ -1,63 +1,39 @@
 /// <reference path="./global.d.ts" />
 //
 // @ts-check
-//
-// The lines above enable type checking for this file. Various IDEs interpret
-// the @ts-check and reference directives. Together, they give you helpful
-// autocompletion when implementing this exercise. You don't need to understand
-// them in order to use it.
-//
-// In your own projects, files, and code, you can play with @ts-check as well.
 
-import { checkStatus, checkInventory } from './grocer';
+import { notify } from './notifier';
+import { order } from './grocer';
 
 /**
- * Returns the service status as a boolean value
- * @return {boolean}
+ * @return void
  */
-export function isServiceOnline() {
-  return checkStatus((status) => {
-    return status === "ONLINE";
-  });
+export function onSuccess() {
+    notify({ message: 'SUCCESS' });
 }
 
 /**
- * Pick a fruit using the checkInventory API
- *
+ * @return void
+ */
+export function onError() {
+    notify({ message: 'ERROR' });
+}
+
+/**
+ * @param {GrocerQuery} query
+ * @param {FruitPickerSuccessCallback} onSuccessCallback
+ * @param {FruitPickerErrorCallback} onErrorCallback
+ * @return void
+ */
+export function orderFromGrocer(query, onSuccessCallback, onErrorCallback) {
+    order(query, onSuccessCallback, onErrorCallback);
+}
+
+/**
  * @param {string} variety
  * @param {number} quantity
- * @param {InventoryCallback} callback
- * @return {AvailabilityAction} the result from checkInventory
+ * @return void
  */
-export function pickFruit(variety, quantity, callback) {
-  return checkInventory({ variety: variety, quantity: quantity},callback);
-}
-
-/**
- * This is a callback function to be passed to the checkInventory API
- * handles the next step once the inventory is known
- * @param {string | null} err
- * @param {boolean | undefined} isAvailable
- * @return {AvailabilityAction} whether the fruit was purchased 'PURCHASE' or 'NOOP'
- */
-export function purchaseInventoryIfAvailable(err, isAvailable) {  
-  
-  if (err) {
-    throw Error(err);
-  } 
-  
-  return isAvailable ? "PURCHASE" : "NOOP"; 
-
-  
-}
-
-/**
- * Pick a fruit, and if it is available, purchase it
- *
- * @param {string} variety
- * @param {number} quantity
- * @return {AvailabilityAction} whether the fruit was purchased 'PURCHASE' or 'NOOP'
- */
-export function pickAndPurchaseFruit(variety, quantity) {
-  return pickFruit(variety, quantity, purchaseInventoryIfAvailable);
+export function postOrder(variety, quantity) {
+    orderFromGrocer({ variety, quantity }, onSuccess, onError);
 }

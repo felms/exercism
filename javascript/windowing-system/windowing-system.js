4b9274f78ec30864ee81857ba070a6b548334a0e
Felipe <felipemoreira1@gmail.com>
2024-04-18 12:59:30 -0300

    JavaScript - Windowing System

--- javascript/windowing-system/windowing-system.js
+++ javascript/windowing-system/windowing-system.js
@@ -1,18 +1,16 @@
 // @ts-check
 
-function Size(width = 80, height = 60) {
+export function Size(width = 80, height = 60) {
     this.width = width;
     this.height = height;
 }
 
-Size.prototype.resize =  function(newWidth, newHeight) {
+Size.prototype.resize = function(newWidth, newHeight) {
     this.width = newWidth;
     this.height = newHeight;
-}
+};
 
-
-
-function Position(x = 0, y = 0) {
+export function Position(x = 0, y = 0) {
     this.x = x;
     this.y = y;
 }
@@ -20,49 +18,64 @@ function Position(x = 0, y = 0) {
 Position.prototype.move = function(newX, newY) {
     this.x = newX;
     this.y = newY;
-}
+};
 
+export class ProgramWindow {
 
+    constructor () {
+        this._screenSize = new Size(800, 600);
+        this._size = new Size();
+        this._position = new Position();
+    }
 
-class ProgramWindow {
-    
-    constructor() {
-        this.screenSize = new Size(800, 600);
-        this.size = new Size();
-        this.position = new Position();
+    get screenSize() {
+        return this._screenSize;
+    }
+
+    get size() {
+        return this._size;
+    }
+
+    set size(newSize) {
+        this._size = newSize;
+    }
+
+    get position() {
+        return this._position;
+    }
+
+    set position(newPosition) {
+        this._position = newPosition;
     }
 
     resize(newSize) {
-        let w = newSize.width < 1 ? 1 : newSize.width;;
-        let h = newSize.height < 1 ? 1 : newSize.height;
-        
-        w = this.position.x + w > this.screenSize.width ? this.screenSize.width - this.position.x : w;
-        h = this.position.y + h > this.screenSize.height ? this.screenSize.height - this.position.y : h;
+        let newWidth = newSize.width < 1 ? 1 : newSize.width;
+        newWidth = newWidth + this._position.x > this._screenSize.width 
+            ? this.screenSize.width - this._position.x : newWidth;
 
-        this.size = new Size(w,h);
+        let newHeight = newSize.height < 1 ? 1 : newSize.height;
+        newHeight = newHeight + this._position.y > this._screenSize.height 
+            ? this.screenSize.height - this._position.y : newHeight;
+
+        this._size = new Size(newWidth, newHeight);
+
     }
 
     move(newPosition) {
-        let x = newPosition.x < 0 ? 0 : newPosition.x;
-        let y = newPosition.y < 0 ? 0 : newPosition.y;
+        let newX = newPosition.x < 0 ? 0 : newPosition.x;
+        newX = newX + this._size.width > this._screenSize.width
+            ? this._screenSize.width - this._size.width : newX;
 
-        x = x + this.size.width > this.screenSize.width ? this.screenSize.width - this.size.width : x;
-        y = y + this.size.height > this.screenSize.height ? this.screenSize.height - this.size.height : y;
+        let newY = newPosition.y < 0 ? 0 : newPosition.y;
+        newY = newY + this._size.height > this._screenSize.height
+            ? this._screenSize.height - this._size.height : newY;
 
-        this.position = new Position(x, y);
+        this._position = new Position(newX, newY);
     }
 
 }
-
-
-
-function changeWindow(newProgramWindow) {
-    let pSize = new Size(400, 300);
-    let pPosition = new Position(100, 150);
-    newProgramWindow.resize(pSize);
-    newProgramWindow.move(pPosition);
-
-    return newProgramWindow;
-}
-
-export {Size, Position, ProgramWindow, changeWindow};
+export const changeWindow = (programWindow) => {
+    programWindow.size = new Size(400, 300);
+    programWindow.position = new Position(100, 150);
+    return programWindow;
+};

8be9b20d3ea531abc8310157c27510e8c2dbd376
Felipe <felipemoreira1@gmail.com>
2024-04-19 13:59:54 -0300

    Java - Collatz Conjecture

--- java/collatz-conjecture/src/main/java/CollatzCalculator.java
+++ java/collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -1,27 +1,12 @@
 class CollatzCalculator {
 
     int computeStepCount(int start) {
-
-        if (start < 1) {
-            throw new IllegalArgumentException("Only natural numbers are allowed");
-        }
-
-        int steps = 0;
-        int number = start;
         
-        while (number > 1) {
-            if (number % 2 == 0) {
-                number /= 2;
-            } else {
-                number *= 3;
-                number++;
-            }
-
-            steps++;
-
+        if (start < 1) {
+            throw new IllegalArgumentException("Only positive integers are allowed");
         }
 
-        return steps;
+        return start == 1 ? 0 : 1 + computeStepCount(start % 2 == 0 ? start / 2 : 3 * start + 1);
     }
 
 }

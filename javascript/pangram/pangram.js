265f5ffa20ce21d90046117134860fbe725f6eb0
Felipe <felipemoreira1@gmail.com>
2024-04-23 08:57:29 -0300

    JavaScript - Pangram

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,17 +1,4 @@
-//
-// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
 
-export const isPangram = (word) => {
-  word = word.toLowerCase();
-  let alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-  for (let i = 0; i < alphabet.length; i++) {
-    if (word.indexOf(alphabet[i]) < 0) {
-      return false;
-    }
-  }
-
-  return true;
-};
+export const isPangram = (word) => 
+                [...ALPHABET].every(letter => word.toLowerCase().includes(letter));

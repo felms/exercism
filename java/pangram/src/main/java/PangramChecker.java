cc9781961ff2e0797e6cde9c0b579ebecc536dfd
Felipe <felipemoreira1@gmail.com>
2024-04-23 09:14:37 -0300

    Java - Pangram

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,18 +1,13 @@
 import java.util.Arrays;
-import java.util.List;
 
 public class PangramChecker {
 
-    private final List<String> alphabet;
-
-    public PangramChecker(){
-        this.alphabet = Arrays.asList("abcdefghijklmnopqrstuvwxyz".split("")); 
-    }
-
-    
     public boolean isPangram(String input) {
-        return Arrays.asList(input.toLowerCase().split(""))
-			.containsAll(this.alphabet);
+        String[] alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+        return Arrays.stream(alphabet)
+                .allMatch(letter -> input.toLowerCase().contains(letter));
+
     }
 
 }

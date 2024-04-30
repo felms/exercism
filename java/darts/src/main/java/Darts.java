f45654c904d6c7762a346859d2e0b211fe0f9143
Felipe <felipemoreira1@gmail.com>
2024-04-30 08:27:47 -0300

    Java - Darts

--- java/darts/src/main/java/Darts.java
+++ java/darts/src/main/java/Darts.java
@@ -1,24 +1,7 @@
 class Darts {
+    int score(double xOfDart, double yOfDart) {
+        double distance = Math.sqrt(Math.pow(xOfDart, 2) + Math.pow(yOfDart, 2));
 
-    private double x;
-    private double y;
-
-    Darts(double x, double y) {
-        this.x = x;
-        this.y = y;
+        return distance <= 1 ? 10 : distance <= 5 ? 5 : distance <= 10 ? 1 : 0;
     }
-
-    int score() {
-        double distance = distanceBetweenPoints(0, 0, this.x, this.y);
-        return distance <= 1 ? 10 
-                : distance <= 5 ? 5
-                : distance <= 10 ? 1
-                : 0;
-    }
-
-    private double distanceBetweenPoints(double x1, double y1, double x2, double y2) {
-
-        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-    }
-
 }

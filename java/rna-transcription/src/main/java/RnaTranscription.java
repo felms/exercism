c94aac4f848917123d07968e030fcf08a26dfab5
Felipe <felipemoreira1@gmail.com>
2024-05-07 10:20:17 -0300

    Java - RNA Transcription

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,38 +1,19 @@
-import java.util.Arrays;
 import java.util.stream.Collectors;
 
 class RnaTranscription {
 
     String transcribe(String dnaStrand) {
-        return Arrays.asList(dnaStrand.split(""))
-                        .stream()
-                        .map(this::getComplement)
-                        .collect(Collectors.joining());
-    }
-
-    private String getComplement(String nucleotide) {
-        String result = "";
-
-        switch (nucleotide) {
-            case "G":
-                result = "C";
-                break;
-            case "C":
-                result = "G";
-                break;
-            case "T":
-                result = "A";
-                break;
-            case "A":
-                result = "U";
-                break;
-            default:
-                result = "";
-                break;
-
-        };
-
-        return result;
+        return dnaStrand.chars()
+            .mapToObj(c -> 
+                    switch ((char)c) {
+                        case 'G' -> "C";
+                        case 'C' -> "G";
+                        case 'T' -> "A";
+                        case 'A' -> "U";
+                        default -> "0";
+                    }
+            ) 
+            .collect(Collectors.joining());
     }
 
 }

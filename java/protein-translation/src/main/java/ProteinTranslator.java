cd552a52d9827a720e2f8dee115389dfa71e371c
felipe <felipemoreira1@gmail.com>
2024-05-19 15:59:28 -0300

    Java - Protein Translation

--- java/protein-translation/src/main/java/ProteinTranslator.java
+++ java/protein-translation/src/main/java/ProteinTranslator.java
@@ -1,76 +1,32 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ArrayList;
 
 class ProteinTranslator {
 
     List<String> translate(String rnaSequence) {
-        List<String> proteins = new ArrayList<>();
-        List<String> values = splitSequence(rnaSequence);
-        
-        for (String s : values) { 
-            String protein = getProtein(s);
-
-            if("STOP".equals(protein)) {
-                break;
-            }
-
-            proteins.add(protein);
-        }
-
-        return proteins;
+        return splitString(rnaSequence).stream()
+                .takeWhile(codon -> !codon.equals("UAA") && !codon.equals("UAG") && !codon.equals("UGA"))
+                .map(codon -> switch (codon) {
+                    case "AUG" -> "Methionine";
+                    case "UUU", "UUC" -> "Phenylalanine";
+                    case "UUA", "UUG" -> "Leucine";
+                    case "UCU", "UCC", "UCA", "UCG" -> "Serine";
+                    case "UAU", "UAC" -> "Tyrosine";
+                    case "UGU", "UGC" -> "Cysteine";
+                    case "UGG" -> "Tryptophan";
+                    default -> throw new IllegalArgumentException("Invalid codon");
+                })
+                .toList();
     }
 
-    private List<String> splitSequence(String sequence) {
-        List<String> values = new ArrayList<>();
-        char[] seq = sequence.toCharArray();
+    List<String> splitString(String input) {
+        List<String> res = new ArrayList<>();
+        int size = input.length();
 
-        for (int i = 0; i < seq.length; i += 3) {
-            values.add(new String(seq, i, 3));
+        for (int i = 0; i < size; i += 3) {
+            res.add(input.substring(i, Math.min(size, i + 3)));
         }
 
-        return values;
-    }
-
-    private String getProtein(String s) {
-        String p = "";
-
-        switch(s) {
-            case "AUG":
-                p = "Methionine";
-                break;
-            case "UUU":
-            case "UUC":
-                p = "Phenylalanine";
-                break;
-            case "UUA":
-            case "UUG":
-                p = "Leucine";
-                break;
-            case "UCU":
-            case "UCC":
-            case "UCA":
-            case "UCG":
-                p = "Serine";
-                break;
-            case "UAU":
-            case "UAC":
-                p = "Tyrosine";
-                break;
-            case "UGU":
-            case "UGC":
-                p = "Cysteine";
-                break;
-            case "UGG":
-                p = "Tryptophan";
-                break;
-            case "UAA":
-            case "UAG":
-            case "UGA":
-                p = "STOP";
-
-        }
-
-        return p;
+        return res;
     }
 }

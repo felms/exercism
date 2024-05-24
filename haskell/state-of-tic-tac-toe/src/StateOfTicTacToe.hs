403bb90d981d48851bd21510f1c1b7e7b9134e41
Felipe <felipemoreira1@gmail.com>
2024-05-24 13:09:54 -0300

    Haskell - State of Tic-Tac-Toe

--- haskell/state-of-tic-tac-toe/src/StateOfTicTacToe.hs
+++ haskell/state-of-tic-tac-toe/src/StateOfTicTacToe.hs
@@ -1,46 +1,38 @@
 module StateOfTicTacToe (gameState, GameState(..)) where
 
+import Data.List
+
 data GameState = WinX | WinO | Draw | Ongoing | Impossible deriving (Eq, Show)
 
 gameState :: [String] -> GameState
 gameState board
-    | impossible . concat $ board = Impossible
-    | xWon . concat $ board = WinX
-    | oWon . concat $ board = WinO
-    | draw . concat $ board = Draw
-    | otherwise = Ongoing
+    | impossible board    = Impossible
+    | playerWon 'X' board = WinX
+    | playerWon 'O' board = WinO
+    | draw board          = Draw
+    | otherwise           = Ongoing
 
-xWon :: String -> Bool
-xWon ['X', 'X', 'X', _, _, _, _, _, _] = True
-xWon [_, _, _, 'X', 'X', 'X', _, _, _] = True
-xWon [_, _, _, _, _, _, 'X', 'X', 'X'] = True
-xWon ['X', _, _, 'X', _, _, 'X', _, _] = True
-xWon [_, 'X', _, _, 'X', _, _, 'X', _] = True
-xWon [_, _, 'X', _, _, 'X', _, _, 'X'] = True
-xWon ['X', _, _, _, 'X', _, _, _, 'X'] = True
-xWon [_, _, 'X', _, 'X', _, 'X', _, _] = True
-xWon _ = False
+playerWon :: Char -> [String] -> Bool
+playerWon player board = 
+    let element = [player, player, player] in
+        element `elem` board ||
+        element `elem` transpose board ||
+        element == (diagonal board) ||
+        element == (diagonal $ map reverse board)
 
-oWon :: String -> Bool
-oWon ['O', 'O', 'O', _, _, _, _, _, _] = True
-oWon [_, _, _, 'O', 'O', 'O', _, _, _] = True
-oWon [_, _, _, _, _, _, 'O', 'O', 'O'] = True
-oWon ['O', _, _, 'O', _, _, 'O', _, _] = True
-oWon [_, 'O', _, _, 'O', _, _, 'O', _] = True
-oWon [_, _, 'O', _, _, 'O', _, _, 'O'] = True
-oWon ['O', _, _, _, 'O', _, _, _, 'O'] = True
-oWon [_, _, 'O', _, 'O', _, 'O', _, _] = True
-oWon _ = False
+draw :: [String] -> Bool
+draw board = (count ' ' board) == 0
 
-draw :: String -> Bool
-draw board = count ' ' board == 0
-
-impossible :: String -> Bool
+impossible :: [String] -> Bool
 impossible board
     | (count 'X' board) - (count 'O' board) > 1 = True
     | (count 'X' board) < (count 'O' board) = True
-    | (xWon board) && (oWon board) = True
+    | (playerWon 'X' board) && (playerWon 'O' board) = True
     | otherwise = False
 
-count :: Char -> String -> Int
-count item board = length $ filter (== item) $ board
+count :: Char -> [String] -> Int
+count item board = length $ filter (== item) $ concat board
+
+diagonal :: [[a]] -> [a]
+diagonal [] = []
+diagonal (x : xs) = head x : (diagonal $ map tail xs)

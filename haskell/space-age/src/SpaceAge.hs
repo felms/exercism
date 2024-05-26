47f2be6e20d524a3839db4cf98ecce693936e78b
felipe <felipemoreira1@gmail.com>
2024-05-26 11:59:26 -0300

    Haskell - Space Age

---
+++ haskell/space-age/src/SpaceAge.hs
@@ -0,0 +1,24 @@
+module SpaceAge (Planet(..), ageOn) where
+
+data Planet = Mercury
+            | Venus
+            | Earth
+            | Mars
+            | Jupiter
+            | Saturn
+            | Uranus
+            | Neptune
+
+ageOn :: Planet -> Float -> Float
+ageOn Mercury seconds = (ageOnEarth seconds) / 0.2408467 
+ageOn Venus seconds = (ageOnEarth seconds) / 0.61519726
+ageOn Earth seconds = ageOnEarth seconds
+ageOn Mars seconds = (ageOnEarth seconds) / 1.8808158
+ageOn Jupiter seconds = (ageOnEarth seconds) / 11.862615
+ageOn Saturn seconds = (ageOnEarth seconds) / 29.447498
+ageOn Uranus seconds = (ageOnEarth seconds) / 84.016846
+ageOn Neptune seconds = (ageOnEarth seconds) / 164.79132
+
+
+ageOnEarth :: Float -> Float
+ageOnEarth seconds = seconds / 31557600

bca017f43bafbdf00797aa7845f119e733eb0b6a
Felipe <felipemoreira1@gmail.com>
2024-05-29 10:07:06 -0300

    Java - Pig Latin

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,54 +1,26 @@
-public class PigLatinTranslator {
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
-    public String translate(String input) {
+class PigLatinTranslator {
 
-        StringBuilder sb = new StringBuilder();
+    private String regex = "(^[aeiou]|^xr|^y[^aeiou]).*";
 
-        String[] words = input.split("\\s+");
+    public String translate(String word) {
+        return Arrays.stream(word.split(" "))
+                .map(this::translateWord).collect(Collectors.joining(" "));
+    }
 
-        for (String word : words) {
-            if (word.matches("^(xr|yt).*|^[aeiou].*")) {
-                sb.append(rule1(word));
-            } else if (word.matches("[^aeiou]*qu.*")) {
-                sb.append(rule3(word));
-            } else if ((word.length() == 2 && word.charAt(1) == 'y') 
-                || word.matches("^[^aeiou]+y.*")) {
-                sb.append(rule4(word));
-            } else if (word.matches("^([^aeiou]+).*")) {
-                sb.append(rule2(word));
-            }
+    public String translateWord(String word) {
 
-            sb.append(" ");
+        if (word.matches(regex)) {
+            return word + "ay";
         }
 
-        return sb.toString().trim();
+        String newWord = word.matches("^qu.*")
+                ? word.substring(2) + "qu" 
+                : word.substring(1) + word.substring(0, 1);
+
+        return translateWord(newWord);
     }
 
-    private String rule1(String input) {
-        return input + "ay";
-    }
-
-    private String rule2(String input) {
-        String consonants = input.split("[aeiou]")[0];
-        String endOfWord = input.substring(consonants.length());
-
-        return endOfWord + consonants + "ay";
-    }
-
-    private String rule3(String input) {
-        String[] tokens = input.split(".*qu");
-        String endOfWord = tokens[tokens.length - 1];
-        String consonants = input.replace(endOfWord, "");
-
-        return endOfWord + consonants + "ay";
-    }
-
-    private String rule4(String input) {
-        String[] tokens = input.split("y");
-        String consonants = tokens[0];
-        String endOfWord = input.replace(consonants, "");
-
-        return endOfWord + consonants + "ay";
-        
-    }
 }

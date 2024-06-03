805a6ac32563f8cacc073da497ff01257b4510f8
Felipe <felipemoreira1@gmail.com>
2024-06-03 12:38:44 -0300

    JavaScript - Space Age

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,40 +1,36 @@
-//
-// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const EARTH_YEAR = 31557600;
 
-export const age = (planet, ageInSeconds) => {
-  const earthYear = 31557600;
+export const age = (planet, timeInSeconds) => {
+    let orbitalPeriod = 0.0;
+    switch (planet) {
+        case 'mercury':
+            orbitalPeriod = 0.2408467;
+            break;
+        case 'venus':
+            orbitalPeriod = 0.61519726;
+            break;
+        case 'earth':
+            orbitalPeriod = 1.0;
+            break;
+        case 'mars':
+            orbitalPeriod = 1.8808158;
+            break;
+        case 'jupiter':
+            orbitalPeriod = 11.862615;
+            break;
+        case 'saturn':
+            orbitalPeriod = 29.447498;
+            break;
+        case 'uranus':
+            orbitalPeriod = 84.016846;
+            break;
+        case 'neptune':
+            orbitalPeriod = 164.79132;
+            break;
 
-  let age = 0.0;
+    };
 
-  switch(planet){
-    case "earth":
-      age = (ageInSeconds / earthYear) / 1.0;
-      break;  
-    case "mercury":
-      age = (ageInSeconds / earthYear) / 0.2408467;
-      break;  
-    case "venus":
-      age = (ageInSeconds / earthYear) / 0.61519726;
-      break;  
-    case "mars":
-      age = (ageInSeconds / earthYear) / 1.8808158;
-      break;  
-    case "jupiter":
-      age = (ageInSeconds / earthYear) / 11.862615;
-      break;  
-    case "saturn":
-      age = (ageInSeconds / earthYear) / 29.447498;
-      break;  
-    case "uranus":
-      age = (ageInSeconds / earthYear) / 84.016846;
-      break;  
-    case "neptune":
-      age = (ageInSeconds / earthYear) / 164.79132;
-      break;
-    }
+    let age = (timeInSeconds / EARTH_YEAR) / orbitalPeriod;
 
     return Number(age.toFixed(2));
-  
 };

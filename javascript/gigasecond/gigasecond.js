d69f43383ab602d840f0e34c3e089c3bd2b7e54c
Felipe <felipemoreira1@gmail.com>
2024-06-03 09:11:41 -0300

    JavaScript - Gigasecond

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,11 +1,6 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const GIGASECOND = 1_000_000_000_000;
 
-export const gigasecond = (inputDate) => {
-  const milis = 1_000_000_000_000 + inputDate.getTime();
-  const newDate = new Date();
-  newDate.setTime(milis);
-  return newDate;
+export const gigasecond = (date) => {
+    let newDate = new Date(date.getTime() + GIGASECOND);
+    return newDate;
 };

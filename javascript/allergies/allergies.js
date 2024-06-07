efeb0c3565fbe2d308b433f01b8b974e3f318859
Felipe <felipemoreira1@gmail.com>
2024-06-07 10:22:46 -0300

    JavaScript - Allergies

--- javascript/allergies/allergies.js
+++ javascript/allergies/allergies.js
@@ -1,58 +1,26 @@
-//
-// This is only a SKELETON file for the 'Allergies' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const allergens = {
+        'eggs' :1,
+        'peanuts' :2,
+        'shellfish' :4,
+        'strawberries' :8,
+        'tomatoes' :16,
+        'chocolate' :32,
+        'pollen' :64,
+        'cats' :128
+    };
 
 export class Allergies {
-  constructor(score) {
-    this.allergens = [];
-    let binaryScore = Number.parseInt(score.toString(2));
-    if (binaryScore % 2 != 0) {
-      this.allergens.push("eggs");
+
+    constructor(score) {
+        this.score = score;
     }
-    binaryScore = Math.floor(binaryScore / 10);
 
-    if (binaryScore % 2 != 0) {
-      this.allergens.push("peanuts");
+    list() {
+        return Object.keys(allergens)
+            .filter(allergen => this.allergicTo(allergen));
     }
-    binaryScore = Math.floor(binaryScore / 10);
 
-    if (binaryScore % 2 != 0) {
-        this.allergens.push("shellfish");
+    allergicTo(allergen) {
+        return (this.score & allergens[allergen]) != 0;
     }
-    binaryScore = Math.floor(binaryScore / 10);
-
-    if (binaryScore % 2 != 0) {
-        this.allergens.push("strawberries");
-    }
-    binaryScore = Math.floor(binaryScore / 10);
-
-    if (binaryScore % 2 != 0) {
-        this.allergens.push("tomatoes");
-    }
-    binaryScore = Math.floor(binaryScore / 10);
-
-    if (binaryScore % 2 != 0) {
-        this.allergens.push("chocolate");
-    }
-    binaryScore = Math.floor(binaryScore / 10);
-
-    if (binaryScore % 2 != 0) {
-        this.allergens.push("pollen");
-    }
-    binaryScore = Math.floor(binaryScore / 10);
-
-    if (binaryScore % 2 != 0) {
-        this.allergens.push("cats");
-    }
-    binaryScore = Math.floor(binaryScore / 10);
-  }
-
-  list() {
-    return this.allergens;
-  }
-
-  allergicTo(allergen) {
-    return this.allergens.includes(allergen);
-  }
 }

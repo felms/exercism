eaeb6ecf8613094ff4f1cf4de5ebe08be66481ea
Felipe <felipemoreira1@gmail.com>
2024-06-20 16:26:31 -0300

    Visual Basic - Matching Brackets

---
+++ vbnet/matching-brackets/MatchingBrackets.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Collections.Generic
+
+Public Module MatchingBrackets
+    Public Function IsPaired(ByVal input As String) As Boolean
+
+        Dim stack(Len(input)) as String
+        Dim pos As Integer = 0
+
+        For Each c As Char In input
+            If c = "(" Or c = "[" Or c = "{" Then
+                stack(pos) = c
+                pos += 1
+            ElseIf c = ")" Or c = "]" Or c = "}" Then
+                pos -= 1
+
+                If pos < 0 Then
+                    Return False
+                ElseIf c = ")" And stack(pos) <> "(" Then
+                    Return False
+                ElseIf c = "]" And stack(pos) <> "[" Then
+                    Return False
+                ElseIf c = "}" And stack(pos) <> "{" Then
+                    Return False
+                End If
+            End If
+        Next
+
+        Return pos = 0
+
+    End Function
+End Module

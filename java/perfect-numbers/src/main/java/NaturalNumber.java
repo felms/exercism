a2d2fce514f937423236fdb9c804adac8f4720c0
Felipe <felipemoreira1@gmail.com>
2024-06-24 13:11:31 -0300

    Java - Perfect Numbers

--- java/perfect-numbers/src/main/java/NaturalNumber.java
+++ java/perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,25 +1,28 @@
 import java.util.stream.IntStream;
 
-public class NaturalNumber {
+class NaturalNumber {
 
     private int number;
 
-    public NaturalNumber(int number) {
+    NaturalNumber(int number) {
+
         if (number < 1) {
-            throw new IllegalArgumentException("You must supply a natural number (positive integer)");
+            throw new IllegalArgumentException(
+                "You must supply a natural number (positive integer)");
         }
 
         this.number = number;
-
     }
 
-    public Classification getClassification() {
+    Classification getClassification() {
+        int sum = this.aliquotSum();
+        return sum == this.number ? Classification.PERFECT 
+                        : sum > this.number ? Classification.ABUNDANT
+                        : Classification.DEFICIENT;
+    }
 
-        int alicotSum = IntStream.range(1, (this.number / 2) + 1)
-                            .filter(factor -> this.number % factor == 0)
-                            .sum();
-
-        return alicotSum == this.number ? Classification.PERFECT
-                             : alicotSum < this.number ? Classification.DEFICIENT : Classification.ABUNDANT;
+    private int aliquotSum() {
+        return IntStream.range(1, (this.number) / 2 + 1)
+                .filter(n -> this.number % n == 0).sum();
     }
 }

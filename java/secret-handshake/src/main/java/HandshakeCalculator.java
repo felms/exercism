79bc443bd6416aae8e82cb3fcdd9f43fe2bc646b
Felipe <felipemoreira1@gmail.com>
2024-06-27 16:09:24 -0300

    Java - Secret Handshake

--- java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,20 +1,19 @@
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class HandshakeCalculator {
 
     List<Signal> calculateHandshake(int number) {
+        Signal[] signals = Signal.values();
 
-        List<Signal> res = new ArrayList<>();
+        List<Signal> res = IntStream.range(0, signals.length)
+                    .filter(i -> (number & (1 << i)) > 0)
+                    .mapToObj(i -> signals[i])
+                    .collect(Collectors.toList());
 
-        for (Signal s : Signal.values()) {
-            if (((number >> s.ordinal()) & 1) == 1) {
-                res.add(s);
-            }
-        }
-
-        if (((number >> 4) & 1) == 1) {
+        if ((number & 0b10000) > 0) {
             Collections.reverse(res);
         }
 

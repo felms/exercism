3a0bef5bf3ea373b1ed0660c2ff46b8a8db59534
Felipe <felipemoreira1@gmail.com>
2024-07-05 09:58:18 -0300

    Java - Largest Series Product

--- java/largest-series-product/src/main/java/LargestSeriesProductCalculator.java
+++ java/largest-series-product/src/main/java/LargestSeriesProductCalculator.java
@@ -1,40 +1,36 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class LargestSeriesProductCalculator {
 
-    private final String number;
+    private String inputNumber;
 
     LargestSeriesProductCalculator(String inputNumber) {
-        if (inputNumber.matches(".*[^\\d].*")) {
+        if (inputNumber.matches(".*\\D.*")) {
             throw new IllegalArgumentException("String to search may only contain digits.");
         }
-        this.number = inputNumber;
+
+        this.inputNumber = inputNumber;
     }
 
     long calculateLargestProductForSeriesLength(int numberOfDigits) {
-
-        if (numberOfDigits > this.number.length()) {
-            throw new IllegalArgumentException("Series length must be less than or equal to the length of the string to search.");
-        }
-
         if (numberOfDigits < 0) {
             throw new IllegalArgumentException("Series length must be non-negative.");
         }
 
-        if (this.number.length() < 1 || numberOfDigits == 0) {
-            return 1;
+        if (numberOfDigits > this.inputNumber.length()) {
+            throw new IllegalArgumentException("Series length must be less than or equal to the length of the string to search.");
         }
 
-        long largestProduct = Long.MIN_VALUE;
-        for (int i = 0; i <= number.length() - numberOfDigits; i++) {
-            String subs = this.number.substring(i, i + numberOfDigits);
-            long product = Arrays.stream(subs.split(""))
-                    .mapToLong(Long::parseLong)
-                    .reduce(1, (a, b) -> a * b);
-            largestProduct = Math.max(largestProduct, product);
-        }
+        return IntStream.range(0, (this.inputNumber.length() - numberOfDigits + 1))
+            .mapToObj(pos -> this.inputNumber.substring(pos, pos + numberOfDigits))
+            .mapToLong(this::product).max().getAsLong();
 
-        return largestProduct;
-    
+    }
+
+    private long product(String inputNumber) {
+        return Arrays.stream(inputNumber.split(""))
+                .mapToLong(Integer::parseInt)
+                .reduce((a, b) -> a * b).getAsLong();
     }
 }

d3033954226ad1449ba065a96f11c86c26fa6528
Felipe <felipemoreira1@gmail.com>
2024-07-05 13:07:57 -0300

    JavaScript - Largest Series Product

--- javascript/largest-series-product/largest-series-product.js
+++ javascript/largest-series-product/largest-series-product.js
@@ -1,29 +1,21 @@
-export const largestProduct = (string, numberOfDigits) => {
-
-    if (string.length === 0 && numberOfDigits === 0) {
-        return 1;
-    }
-
-    if (string.length === 0 || numberOfDigits > string.length) {
-        throw new Error('Span must be smaller than string length');
-    }
-
-    if (numberOfDigits < 0) {
-        throw new Error('Span must be greater than zero'); 
-    }
-
-    if ((/[^\d]/.test(string))) {
+export const largestProduct = (inputNumber, numberOfDigits) => {
+    if (/\D/.test(inputNumber)) {
         throw new Error('Digits input must only contain digits');
     }
 
-    let digitsArray = string.split('').map(digit => parseInt(digit));
-
-    let largestProduct = 0; 
-    for (let i = 0; i <= digitsArray.length - numberOfDigits; i++) {
-        let subArray = digitsArray.slice(i, i + numberOfDigits);
-        let product = subArray.reduce((acc, curr) => acc * curr, 1);
-        largestProduct = Math.max(largestProduct, product);
+    if (numberOfDigits <= 0) {
+        throw new Error('Span must be greater than zero');
     }
 
-    return largestProduct;
+    if (inputNumber.length < numberOfDigits) {
+        throw new Error('Span must be smaller than string length');
+
+    }
+
+    return [...Array(inputNumber.length - numberOfDigits + 1).keys()]
+        .map(pos => product(inputNumber.slice(pos, pos + numberOfDigits)))
+        .reduce((acc, current) => Math.max(acc, current));
 };
+
+const product = (inputNumber) => 
+    [...inputNumber].map(Number).reduce((a, b) => a * b, 1);

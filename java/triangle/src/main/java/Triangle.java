3c83652fa8a81aabbb8e343ff7178033d38df72b
Felipe <felipemoreira1@gmail.com>
2024-07-08 13:21:42 -0300

    Java - Triangle

--- java/triangle/src/main/java/Triangle.java
+++ java/triangle/src/main/java/Triangle.java
@@ -5,32 +5,35 @@ class Triangle {
     private double side3;
 
     Triangle(double side1, double side2, double side3) throws TriangleException {
-        if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
-            throw new TriangleException();
-        }
-
-        if (side1 + side2 < side3 
-            || side1 + side3 < side2 
-            || side2 + side3 < side1) {
-            throw new TriangleException();
-        }
 
         this.side1 = side1;
         this.side2 = side2;
         this.side3 = side3;
 
+        if (!this.isTriangle()) {
+            throw new TriangleException();
+        }
     }
 
     boolean isEquilateral() {
-        return side1 == side2 && side1 == side3;
+        return this.side1 == this.side2 && this.side2 == this.side3;
     }
 
     boolean isIsosceles() {
-        return side1 == side2 || side1 == side3 || side2 == side3;
+        return this.side1 == this.side2 
+                || this.side1 == this.side3 || this.side2 == this.side3;
     }
 
     boolean isScalene() {
-        return side1 != side2 && side1 != side3 && side2 != side3;
+        return this.side1 != this.side2 
+                && this.side1 != this.side3 && this.side2 != this.side3;
+    }
+
+    private boolean isTriangle() {
+        return ((this.side1 > 0 && side2 > 0 && side3 > 0) 
+            && (side1 + side2 >= side3 
+                && side1 + side3 >= side2
+                && side2 + side3 >= side1));
     }
 
 }

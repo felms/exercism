7de13671b2811d93402f1d4dcb3e8700abafd377
Felipe <felipemoreira1@gmail.com>
2024-07-09 15:05:28 -0300

    Java - Spiral Matrix

--- java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
+++ java/spiral-matrix/src/main/java/SpiralMatrixBuilder.java
@@ -1,78 +1,68 @@
-public class SpiralMatrixBuilder {
-    
-    public enum Direction {
-        LEFT, RIGHT, UP, DOWN
-    } 
+import java.util.Arrays;
 
-    public int[][] buildMatrixOfSize(int size) {
-        
-        if (size == 0) {
+class SpiralMatrixBuilder {
+    int[][] buildMatrixOfSize(int size) {
 
-            return new int[][]{};
-        }
-        
-        int[][] matrix = initMatrix(size);
-        
-        int i = 0;
-        int j = 0;
+        int lastNumber = size * size;
         int currentNumber = 1;
-        int maxNumber = size * size;
+        int r = 0;
+        int c = 0;
+
         Direction direction = Direction.RIGHT;
 
-        while (currentNumber <= maxNumber) {
-            matrix[i][j] = currentNumber;
-            currentNumber++;
-            
-            if (direction == Direction.RIGHT) {
-                if (j == size - 1
-                        || matrix[i][j + 1] != -1) {
-                    direction = Direction.DOWN;
-                    i++;
-                } else {
-                    j++;
-                } 
-            } else if (direction == Direction.DOWN) {
-                if (i == size - 1
-                        || matrix[i + 1][j] != -1) {
-                    direction = Direction.LEFT;
-                    j--;
-                } else {
-                    i++;
-                }
-            } else if (direction == Direction.LEFT) {
-                if (j == 0
-                        || matrix[i][j - 1] != -1) {
-                    direction = Direction.UP;
-                    i--;
-                } else {
-                    j--;
-                }
-            } else if (direction == Direction.UP) {
-                if (i == 0
-                        || matrix[i - 1][j] != -1) {
-                    direction = Direction.RIGHT;
-                    j++;
-                } else {
-                    i--;
-                }
-            }
+        int[][] matrix = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            Arrays.fill(matrix[i], -1);
+        }
 
-        } 
+        while (currentNumber <= lastNumber) {
+
+            matrix[r][c] = currentNumber;
+            currentNumber++;
+
+            if(direction == Direction.RIGHT) {
+
+                if (c == size - 1 || matrix[r][c + 1] != -1) {
+                    r++;
+                    direction = Direction.DOWN;
+                } else {
+                    c++;
+                }
+
+            } else if (direction == Direction.DOWN) {
+
+                if (r == size - 1 || matrix[r + 1][c] != -1) {
+                    c--;
+                    direction = Direction.LEFT;
+                } else {
+                    r++;
+                }
+
+            } else if (direction == Direction.LEFT) {
+
+                if (c == 0 || matrix[r][c - 1] != -1) {
+                    r--;
+                    direction = Direction.UP;
+                } else {
+                    c--;
+                }
+
+            } else { // direction == Direction.UP
+
+                if (r == 0 || matrix[r - 1][c] != -1) {
+                    c++;
+                    direction = Direction.RIGHT;
+                } else {
+                    r--;
+                }
+
+            }
+        }
 
         return matrix;
     }
-    
-    private int[][] initMatrix(int size) {
 
-        int[][] matrix = new int[size][size];
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                matrix[i][j] = -1;
-            }
-        }
-        
-        return matrix;
-
+    private enum Direction {
+        UP, DOWN, LEFT, RIGHT;
     }
 }

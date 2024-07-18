bc2c90b940bc45fe2da5ea2829ea696470d78cf7
Felipe <felipemoreira1@gmail.com>
2024-07-18 15:17:45 -0300

    JavaScript - Prime Factors

--- javascript/prime-factors/prime-factors.js
+++ javascript/prime-factors/prime-factors.js
@@ -1,42 +1,16 @@
-
 export const primeFactors = (number) => {
-  let primeFactors = [];
 
-  let n = number;
-  let factor = 0;
+    let factors = [];
+    let factor = 2;
 
-  while (n > 1 && factor <= n) {
-    if (isPrime(factor) && n % factor === 0) {
-      primeFactors.push(factor);
-      n /= factor;
-    } else {
-      factor++;
+    while(number > 1) {
+        while(number % factor === 0) {
+            number /= factor;
+            factors.push(factor);
+        }
+
+        factor++;
     }
-  }
 
-  return primeFactors;
-
+    return factors;
 };
-
-const isPrime = (n) => {
-
-  if (n < 2) {
-    return false;
-  }
-
-  if (n === 2) {
-    return true;
-  }
-
-  if (n % 2 === 0) {
-    return false;
-  }
-
-  for (let i = 3; i <= Math.sqrt(n); i += 2) {
-    if (n % i === 0) {
-      return false;
-    }
-  }
-
-  return true;
-}

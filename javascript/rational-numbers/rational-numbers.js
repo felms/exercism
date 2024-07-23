64fb0c2a275e8784832c83becf57527192fda547
Felipe <felipemoreira1@gmail.com>
2024-07-23 16:35:20 -0300

    JavaScript - Rational Numbers

--- javascript/rational-numbers/rational-numbers.js
+++ javascript/rational-numbers/rational-numbers.js
@@ -1,105 +1,61 @@
 export class Rational {
-  constructor(numerator, denominator) {
 
-    if (denominator === 0) {
-      throw new Error('the denominator can\'t be zero');
+    constructor(numerator, denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
     }
 
-     if (denominator < 0 && numerator >= 0) {
-       numerator *= -1;
-       denominator *= -1;
-     }
-
-    let d = Math.abs(this.gcd(numerator, denominator));
-
-    this.numerator = numerator / d;
-    this.denominator = denominator / d;
-  }
-
-  add(other) {
-    let a1 = this.numerator;
-    let b1 = this.denominator;
-    let a2 = other.numerator;
-    let b2 = other.denominator;
-
-    let newNumerator = a1 * b2 + a2 * b1;
-    let newDenominator = b1 * b2;
-
-    return new Rational(newNumerator, newDenominator);
-  }
-
-  sub(other) {
-    let a1 = this.numerator;
-    let b1 = this.denominator;
-    let a2 = other.numerator;
-    let b2 = other.denominator;
-
-    let newNumerator = a1 * b2 - a2 * b1;
-    let newDenominator = b1 * b2;
-
-    return new Rational(newNumerator, newDenominator);
-  }
-
-  mul(other) {
-    let a1 = this.numerator;
-    let b1 = this.denominator;
-    let a2 = other.numerator;
-    let b2 = other.denominator;
-
-    let newNumerator = a1 * a2;
-    let newDenominator = b1 * b2;
-
-    return new Rational(newNumerator, newDenominator);
-  }
-
-  div(other) {
-    let a1 = this.numerator;
-    let b1 = this.denominator;
-    let a2 = other.numerator;
-    let b2 = other.denominator;
-
-    if (a2 === 0) {
-      throw new Error("The numerator of the second rational can't be zero. (Can't divide by zero)");
+    add(other) {
+        let num = this.numerator * other.denominator + other.numerator * this.denominator;
+        let den = this.denominator * other.denominator;
+        return new Rational(num, den).reduce();
     }
 
-    if (a1 < 0 && a2 < 0) {
-      a1 *= -1;
-      a2 *= -1;
+    sub(other) {
+        let num = this.numerator * other.denominator - other.numerator * this.denominator;
+        let den = this.denominator * other.denominator;
+        return new Rational(num, den).reduce();
     }
 
-    let newNumerator = a1 * b2;
-    let newDenominator = a2 * b1;
-
-    return new Rational(newNumerator, newDenominator);
-  }
-
-  abs() {
-    let a1 = Math.abs(this.numerator);
-    let b1 = Math.abs(this.denominator);
-
-    return new Rational(a1, b1);
-  }
-
-  exprational(exponent) {
-    let m = Math.abs(exponent);
-    let newNumerator = Math.pow(this.numerator, m);
-    let newDenominator = Math.pow(this.denominator, m);
-    return new Rational(newNumerator, newDenominator);
-  }
-
-  expreal(exponent) {
-    return Math.pow(Math.pow(exponent, 1.0 / this.denominator), this.numerator);
-  }
-
-  reduce() {
-    return this;
-  }
-
-  gcd(a, b) {
-    if (b === 0) {
-      return a;
+    mul(other) {
+        let num = this.numerator * other.numerator;
+        let den = this.denominator * other.denominator;
+        return new Rational(num, den).reduce();
     }
 
-    return this.gcd(b, a % b);
-  }
+    div(other) {
+        let num = this.numerator * other.denominator;
+        let den = other.numerator * this.denominator;
+        return new Rational(num, den).reduce();
+    }
+
+    abs() {
+        let n = Math.abs(this.numerator);
+        let d = Math.abs(this.denominator);
+        return new Rational(n, d);
+    }
+
+    exprational(number) {
+        let n = Math.abs(number);
+        return new Rational(this.numerator ** n, this.denominator ** n).reduce();
+    }
+
+    expreal(number) {
+        return Math.pow(Math.pow(number, 1.0 / this.denominator), this.numerator);
+    }
+
+    reduce() {
+        let g = gcd(this.numerator, this.denominator);
+        let n = this.numerator / g;
+        let d = this.denominator / g;
+
+        if (d < 0 && n > 0) {
+            n *= -1;
+            d *= -1;
+        }
+
+        return new Rational(n, d);
+    }
 }
+
+const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);

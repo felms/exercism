a1d9204c2597ebbe300d8fea481d2b58a6a63b0d
Felipe <felipemoreira1@gmail.com>
2024-07-25 11:58:20 -0300

    Java - Anagram

--- java/anagram/src/main/java/Anagram.java
+++ java/anagram/src/main/java/Anagram.java
@@ -1,42 +1,23 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class Anagram{
+class Anagram {
 
     private String word;
 
     public Anagram(String word) {
-        this.word = word;
+        this.word = word.toLowerCase();
     }
 
     public List<String> match(List<String> candidates) {
-        List<String> anagrams = new ArrayList<>();
+        return candidates.stream().filter(this::isAnagram).toList();
+    }
 
-        for ( String str : candidates) {
-            String w = word.toLowerCase();
-            String str0 = str.toLowerCase();
-
-            if (str0.length() == w.length() && !str0.equals(w)) {
-                boolean containsAllLetters = true;
-                for (char c : str0.toCharArray()) {                    
-                    int index = w.indexOf(c);
-                    if (index < 0) {
-                        containsAllLetters = false;
-                    } else {
-                        StringBuilder sb = new StringBuilder(w);
-                        sb.deleteCharAt(index);
-                        w = sb.toString();
-                    }
-                    
-                }
-                if (containsAllLetters) {
-                    anagrams.add(str);
-                }
-            }
-            
-        }
-        
-        return anagrams;
-        
+    private boolean isAnagram(String candidate) {
+        return !candidate.toLowerCase().equals(this.word) 
+                    && Arrays.equals(
+                            candidate.toLowerCase().chars().sorted().toArray(),
+                            this.word.chars().sorted().toArray()
+                        );
     }
 }

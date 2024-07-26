8c5cd03631d8e60bf12fe0c78a11a7dec6a283aa
Felipe <felipemoreira1@gmail.com>
2024-07-26 12:29:14 -0300

    JavaScript - Grains

--- javascript/grains/grains.js
+++ javascript/grains/grains.js
@@ -1,17 +1,17 @@
+/**
+ * You can use the bigint type and BigInt global object to support numbers below
+ * Number.MIN_SAFE_INTEGER and above NUMBER.MAX_SAFE_INTEGER.
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt
+ */
+
 export const square = (squareNumber) => {
-    
-    if (squareNumber < 1 || squareNumber > 64) {
+    if (squareNumber <= 0 || squareNumber > 64) {
         throw new Error('square must be between 1 and 64');
     }
-    
     return 2n ** BigInt(squareNumber - 1);
 };
 
 export const total = () => {
-    
-    let a1 = square(1);
-    let q = 2n;
-    let n = 64n;
-    
-    return a1 * (q ** n - 1n) ;
+    return BigInt(2 ** 64) - 1n;
 };

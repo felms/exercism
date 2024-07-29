8b78a2ac457982af2aef91a86bec5977b539735e
Felipe <felipemoreira1@gmail.com>
2024-07-29 11:48:57 -0300

    JavaScript - Triangle

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -1,40 +1,36 @@
 export class Triangle {
-  constructor(...sides) {
 
-      this.side1 = sides[0];
-      this.side2 = sides[1];
-      this.side3 = sides[2];
-      
-      this.isValidTriangle = function() {
-        if (this.side1 <= 0 || this.side2 <= 0 || this.side3 <= 0) {
-          return false;
-        }
-    
-        if (this.side1 + this.side2 < this.side3 
-            || this.side1 + this.side3 < this.side2 
-            || this.side2 + this.side3 < this.side1) {
-            return false;
-        }
-    
-        return true;
-      }
-      
-  }  
+    #sideA;
+    #sideB;
+    #sideC;
 
-  get isEquilateral() {
-    return this.isValidTriangle() &&
-          (this.side1 === this.side2 && this.side1 === this.side3);
-  }
+    constructor(sideA, sideB, sideC) {
+        this.#sideA = sideA;
+        this.#sideB = sideB;
+        this.#sideC = sideC;
+    }
 
-  get isIsosceles() {
-    return this.isValidTriangle() &&
-          (this.side1 === this.side2 || this.side1 === this.side3 || this.side2 === this.side3);
-  }
+    get isEquilateral() {
+        return this.#isTriangle() && this.#sideA === this.#sideB 
+                                && this.#sideB === this.#sideC;
+    }
 
-  get isScalene() {
-    return this.isValidTriangle() &&
-          (this.side1 !== this.side2 && this.side1 !== this.ide3 && this.side2 !== this.side3);
-  }
+    get isIsosceles() {
+        return this.#isTriangle() && (this.#sideA === this.#sideB 
+                || this.#sideB === this.#sideC || this.#sideA === this.#sideC);
+    }
 
-  
+    get isScalene() {
+        return this.#isTriangle() 
+                && (this.#sideA !== this.#sideB 
+                    && this.#sideA !== this.#sideC 
+                    && this.#sideB !== this.#sideC);
+    }
+
+    #isTriangle() {
+        return this.#sideA > 0 && this.#sideB > 0 && this.#sideC > 0
+                    && ((this.#sideA + this.#sideB >= this.#sideC) &&
+                        (this.#sideB + this.#sideC >= this.#sideA) &&
+                        (this.#sideA + this.#sideC >= this.#sideB)); 
+    }
 }

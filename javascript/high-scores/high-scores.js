539dca0474b3f14deef47c58672a5414270ac111
Felipe <felipemoreira1@gmail.com>
2024-08-08 11:32:59 -0300

    JavaScript - High Scores

--- javascript/high-scores/high-scores.js
+++ javascript/high-scores/high-scores.js
@@ -1,27 +1,24 @@
-//
-// This is only a SKELETON file for the 'High Scores' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class HighScores {
-  constructor(input) {
-    this.scoreList = [...input];
-  }
 
-  get scores() {
-    return this.scoreList;
-  }
+    #scoresList;
 
-  get latest() {
-    return this.scoreList[this.scoreList.length - 1];
-  }
+    constructor(scores) {
+        this.#scoresList = [...scores];
+    }
 
-  get personalBest() {
-    return Math.max(...this.scoreList);
-  }
+    get scores() {
+        return this.#scoresList;
+    }
 
-  get personalTopThree() {
-    let sortedArray = [...this.scoreList].sort((a, b) => b - a);
-    return sortedArray.slice(0, 3);
-  }
+    get latest() {
+        return this.#scoresList[this.#scoresList.length - 1];
+    }
+
+    get personalBest() {
+        return Math.max(...this.#scoresList);
+    }
+
+    get personalTopThree() {
+        return this.#scoresList.toSorted((a, b) => b - a).slice(0, 3);
+    }
 }

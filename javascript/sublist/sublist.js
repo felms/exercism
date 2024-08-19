bea477f345f24e7935ea03e14fa8dc300cb25224
Felipe <felipemoreira1@gmail.com>
2024-08-19 15:55:54 -0300

    JavaScript - Sublist

--- javascript/sublist/sublist.js
+++ javascript/sublist/sublist.js
@@ -1,33 +1,72 @@
 export class List {
-  constructor(items = []) {
-    this._items = items;
-  }
 
-  compare(otherList) {
-    let thisArr = this.values().toString();
-    let otherArr = otherList.values().toString();
-
-    if (thisArr === otherArr) {
-      return 'EQUAL';
+    #items;
+    #size;
+    constructor(items = []) {
+        this.#items = [...items];
+        this.#size = items.length;
     }
 
-    if (thisArr.includes(otherArr)) {
-      return 'SUPERLIST';
+    compare(otherList) {
+        if (List.#equal(this, otherList)) {
+            return 'EQUAL';
+        }
+
+        if (List.#sublist(this, otherList)) {
+            return 'SUBLIST';
+        }
+
+        if (List.#sublist(otherList, this)) {
+            return 'SUPERLIST';
+        }
+
+        return 'UNEQUAL';
     }
 
-    if (otherArr.includes(thisArr)) {
-      return 'SUBLIST';
+    get length() {
+        return this.#size;
     }
 
-    return 'UNEQUAL';
-  }
+    static #equal(listA, listB) {
 
-  size() {
-    return this._items.length;
-  }
+        if (listA.length !== listB.length) {
+            return false;
+        }
 
-  values() {
-    let arr = [...this._items];
-    return arr;
-  }
+        if (listA.length == 0 && listB.length == 0) {
+            return true;
+        }
+
+        let [headA, ...tailA] = listA;
+        let [headB, ...tailB] = listB;
+
+        return headA === headB && List.#equal(tailA, tailB);
+    }
+
+    static #sublist(listA, listB) {
+
+        if (listA.length === 0) {
+            return true;
+        }
+
+        if (listB.length < listA.length) {
+            return false;
+        }
+
+        let s = listA.length;
+        let arr = [...listB];
+        return [...Array(listB.length - s + 1).keys()]
+                .some(i => List.#equal(listA, arr.slice(i, s + i)));
+                
+    }
+
+    [Symbol.iterator]() {
+        var index = -1;
+        var data = this.#items;
+
+        return {
+            next: () => ({ value: data[++index], done: !(index in data) })
+        };
+    }
+
 }

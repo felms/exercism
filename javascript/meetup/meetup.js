782f021856615e29bd42e5450b3b6579d742b2f9
Felipe <felipemoreira1@gmail.com>
2024-08-21 13:37:01 -0300

    JavaScript - Meetup

--- javascript/meetup/meetup.js
+++ javascript/meetup/meetup.js
@@ -1,115 +1,61 @@
-export const meetup = (year, month, descriptor, dayOfweek) => {
+const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 
+    'Thursday', 'Friday', 'Saturday'];
 
-  let day = 1;
-  let dateString = `${year}-${month}-${day}`;
-  const date = new Date(dateString);
-  const desiredDay = getDayOfWeek(dayOfweek);
+const schedulePossibilities = ['first','second', 'third', 'fourth', 'last', 'teenth'];
 
+export const meetup = (year, month, meetupSchedule, dayOfWeek) => {
 
-  switch (descriptor) {
-    case 'first':
-        return handleOffset(date, 0, desiredDay);
-    case 'second':
-        return handleOffset(date, 1, desiredDay);
-    case 'third':
-        return handleOffset(date, 2, desiredDay);
-    case 'fourth':
-        return handleOffset(date, 3, desiredDay);
-    case 'last':
-        return handleLast(date, desiredDay);
-    case 'teenth':
-        return handleTeenth(date, desiredDay);
-  }
+    if (meetupSchedule === 'teenth') {
+        return calcTeenth(year, month, dayOfWeek);
+    }
 
-  return date;
+    if (meetupSchedule === 'last') {
+        return calcLast(year, month, dayOfWeek);
+    }
+
+    return calcDay(year, month, meetupSchedule, dayOfWeek);
 
 };
 
-const handleOffset = (date, weeksToAdd, desiredDay) => {
+const calcTeenth = (year, month, dayOfWeek) => {
 
-  let offset = weeksToAdd * 7;
+    let date = new Date(year, month - 1, 13);
+    let targetDay = dayNames.findIndex(day => day === dayOfWeek);
 
-  if (date.getDay() === desiredDay) {
-    let day = date.getDate();
-    day += offset;
-    date.setDate(day);
+    while (date.getDay() != targetDay) {
+        date.setDate(date.getDate() + 1);
+    }
+
     return date;
-  }
+};
 
-  let daysToAdd = desiredDay - date.getDay();
-  offset += daysToAdd > 0 ? daysToAdd : daysToAdd + 7;
+const calcLast = (year, month, dayOfWeek) => {
 
-  let day = date.getDate();
-  day += offset;
-  date.setDate(day);
-  return date;
+    let date = new Date(year, month, 1); 
+    date.setDate(date.getDate() - 1);
 
-}
+    let targetDay = dayNames.findIndex(day => day === dayOfWeek);
 
-const handleLast = (date, desiredDay) => {
+    while (date.getDay() != targetDay) {
+        date.setDate(date.getDate() - 1);
+    }
 
-  let lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    return date;
+};
 
-  if (lastDayOfMonth.getDay() === desiredDay) {
-    return lastDayOfMonth;
-  }
-  
-  let offset = desiredDay - lastDayOfMonth.getDay();
-  offset = offset > 0 ? offset - 7 : offset;
+const calcDay = (year, month, meetupSchedule, dayOfWeek) => {
 
-  let day = lastDayOfMonth.getDate();
-  day += offset;
-  lastDayOfMonth.setDate(day);
-  return lastDayOfMonth;
+    let date = new Date(year, month - 1, 1); 
 
-}
+    let targetDay = dayNames.findIndex(day => day === dayOfWeek);
 
-const handleTeenth = (date, desiredDay) => {
-  let firstTeenth = new Date(date.getFullYear(), date.getMonth(), 13);
+    while (date.getDay() != targetDay) {
+        date.setDate(date.getDate() + 1);
+    }
 
-  let teenthDayOfWeek = firstTeenth.getDay();
+    let daysToAdd = 7 * schedulePossibilities
+        .findIndex(schedule => schedule === meetupSchedule);
 
-  if (teenthDayOfWeek == desiredDay) {
-    return firstTeenth;
-  }
-
-  let offset = desiredDay - teenthDayOfWeek;
-  offset = offset > 0 ? offset : offset + 7;
-
-  let day = firstTeenth.getDate();
-  day += offset;
-  firstTeenth.setDate(day);
-  return firstTeenth;
-
-}
-
-const getDayOfWeek = (dayOfweek) => {
-
-  let day = -1;
-
-  switch (dayOfweek) {
-    case 'Sunday':
-      day = 0;
-      break;
-    case 'Monday':
-      day = 1;
-      break;
-    case 'Tuesday':
-      day = 2;
-      break;
-    case 'Wednesday':
-      day = 3;
-      break;
-    case 'Thursday':
-      day = 4;
-      break;
-    case 'Friday':
-      day = 5;
-      break;
-    case 'Saturday':
-      day = 6;
-      break;
-  }
-
-  return day;
-}
+    date.setDate(date.getDate() + daysToAdd);
+    return date;
+};

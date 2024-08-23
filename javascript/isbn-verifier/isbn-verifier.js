9cc01b8a034f410a34c81696157df63a0445ba50
Felipe <felipemoreira1@gmail.com>
2024-08-23 11:16:59 -0300

    JavaScript - ISBN Verifier

--- javascript/isbn-verifier/isbn-verifier.js
+++ javascript/isbn-verifier/isbn-verifier.js
@@ -1,22 +1,12 @@
-export const isValid = (isbnCode) => {
-	let code = isbnCode.replaceAll('-', '');
-	
-	if (!(/\d{9}(\d|X)/.test(code))) {
-		return false;
-	}
-	
-	if (code.length != 10) {
-		return false;
-	}
-	
-	let digits = code.split('').map(c => c === 'X' ? 10 : parseInt(c));
-	let sum = 0;
-	
-	for (let i = 0; i < 10; i++) {
-		sum += digits[i] * (10 - i);
-	}
-	
-	return sum % 11 === 0;
-	
-};
+export const isValid = (input) => {
 
+    let isbn = input.replaceAll(/[^X\d]/g, '');
+
+    if (!/^\d{9}[\dX]$/.test(isbn)) {
+        return false;
+    }
+
+    return [...isbn].reduce((acc, number, index) => 
+        ((number === 'X') ? 10 : Number(number)) * (10 - index) + acc, 0) 
+            % 11 === 0;
+};

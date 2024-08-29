250765860516af258298cddb6a66f460094fc61d
Felipe <felipemoreira1@gmail.com>
2024-08-29 10:43:04 -0300

    Erlang - Meetup (WIP:cleanup teenth)

--- erlang/meetup/src/meetup.erl
+++ erlang/meetup/src/meetup.erl
@@ -5,13 +5,13 @@
 
 meetup(Year, Month, DayOfWeek, teenth) -> 
     calc_teenth(calendar:date_to_gregorian_days(Year, Month, 13), day_of_week_to_daynum(DayOfWeek), calendar:day_of_the_week(Year, Month, 13));
-meetup(Year, Month, DayOfWeek, last) -> calc_last(Year, Month, calendar:last_day_of_the_month(Year, Month), day_of_week_to_daynum(DayOfWeek));
+meetup(Year, Month, DayOfWeek, last) -> calc_last(Year, Month, calendar:last_day_of_the_month(Year, Month), DayOfWeek);
 meetup(Year, Month, _DayOfWeek, _) -> {Year, Month, 1}.
 
 
-calc_teenth(GregorianDays, x, x) -> calendar:gregorian_days_to_date(GregorianDays);
-calc_teenth(GregorianDays, DayOfWeek, _DowDate) -> 
-    calc_teenth(GregorianDays + 1, DayOfWeek, calendar:day_of_the_week(calendar:gregorian_days_to_date(GregorianDays + 1))).
+calc_teenth(GregorianDays, X, X) -> calendar:gregorian_days_to_date(GregorianDays);
+calc_teenth(GregorianDays, DayOfWeek, _) -> 
+        calc_teenth(GregorianDays + 1, DayOfWeek, calendar:day_of_the_week(calendar:gregorian_days_to_date(GregorianDays + 1))).
 
 
 calc_last(Year, Month, Day, _DayOfWeek) -> {Year, Month, Day}.

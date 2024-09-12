eaa345e4518ce91a999435c976c6b56cb1cebcd6
Felipe <felipemoreira1@gmail.com>
2024-09-12 11:33:19 -0300

    C# - Hamming

---
+++ csharp/hamming/Hamming.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+
+public static class Hamming
+{
+    public static int Distance(string firstStrand, string secondStrand)
+    {
+        if (firstStrand.Length != secondStrand.Length) 
+        {
+            throw new ArgumentException();
+        }
+
+        return Enumerable.Range(0, firstStrand.Length)
+                .Where(i => firstStrand[i] != secondStrand[i])
+                .Count();
+    }
+}

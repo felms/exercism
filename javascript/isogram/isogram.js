aa3866974e0ee042595a2757da197302f8c6556e
Felipe <felipemoreira1@gmail.com>
2024-09-25 09:15:03 -0300

    JavaScript - Isogram

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -1,18 +1,5 @@
-//
-// This is only a SKELETON file for the 'Isogram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const isIsogram = (word) => {
-  let w = word.replace(/[-\s+]/g, "").toLowerCase().split("");
+    let cleanedWord = word.toLowerCase().replaceAll(/[^a-z]/g, '');
 
-  while(w.length > 0) {
-    let letter = w.shift();
-    if (w.indexOf(letter) >= 0) {
-      return false;
-    }
-  }
-
-  return true;
-
+    return new Set(cleanedWord).size === cleanedWord.length;
 };

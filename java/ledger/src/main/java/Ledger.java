98e4aabe5b75e339fa97484c2ee51b572da33b1f
Felipe <felipemoreira1@gmail.com>
2024-09-26 17:09:43 -0300

    Java - Ledger

--- java/ledger/src/main/java/Ledger.java
+++ java/ledger/src/main/java/Ledger.java
@@ -1,94 +1,81 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols; 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.stream.Collectors;
 
 public class Ledger {
-    public LedgerEntry createLedgerEntry(String d, String desc, double c) {
+
+    private static final String US_LOCALE = "en-US";
+    private static final String NL_LOCALE = "nl-NL";
+
+    private static final String USD_CURRENCY = "USD";
+    private static final String EUR_CURRENCY = "EUR";
+
+    public LedgerEntry createLedgerEntry(String d, String desc, int c) {
         return new LedgerEntry(LocalDate.parse(d), desc, c);
     }
 
     public String format(String cur, String loc, LedgerEntry[] entries) {
 
-        if (!cur.equals("USD") && !cur.equals("EUR")) {
+        if (!cur.equals(USD_CURRENCY) && !cur.equals(EUR_CURRENCY)) {
             throw new IllegalArgumentException("Invalid currency");
-        }
+        } 
 
-        if (!loc.equals("en-US") && !loc.equals("nl-NL")) {
+        if (!loc.equals(US_LOCALE) && !loc.equals(NL_LOCALE)) {
             throw new IllegalArgumentException("Invalid locale");
-        }
+        } 
 
-        Metadata metadata = getMetadata(loc, cur);
+        String header = loc.equals(US_LOCALE) 
+        ? "Date       | Description               | Change       "
+        : "Datum      | Omschrijving              | Verandering  ";
 
-        return metadata.header + Arrays.stream(entries)
-                .sorted(Comparator.comparing(LedgerEntry::localDate)
-                        .thenComparing(LedgerEntry::description)
-                        .thenComparing(LedgerEntry::change))
-                .map(ledgerEntry -> formatEntry(ledgerEntry, metadata))
-                .collect(Collectors.joining());
-    }
+        String curSymb = cur.equals(USD_CURRENCY) ? "$" : "€";
+        String datPat = loc.equals(US_LOCALE) ? "MM/dd/yyyy" : "dd/MM/yyyy";
+        char decSep = loc.equals(US_LOCALE) ? '.' : ',';
+        char thSep = loc.equals(US_LOCALE) ? ',' : '.';
 
-    private static String formatEntry(LedgerEntry ledgerEntry, Metadata metadata) {
+        return header + "\n" +
 
-        String date = ledgerEntry.localDate().format(DateTimeFormatter.ofPattern(metadata.datPat));
+        Arrays.stream(entries)
+        .sorted((o1, o2) -> (o1.change() == o2.change()) 
+            ? o1.localDate().compareTo(o2.localDate()) 
+            : Double.compare(o1.change(), o2.change()))
+        .map((e) -> {
+            // Date
+            String date = e.localDate().format(DateTimeFormatter.ofPattern(datPat));
 
-        String desc = ledgerEntry.description();
-        if (desc.length() > 25) {
-            desc = desc.substring(0, 22);
-            desc = desc + "...";
-        }
-
-        String amount = formatAmount(ledgerEntry, metadata);
-
-        return String.format("\n%s | %-25s | %13s", date, desc, amount);
-    }
-
-    private static String formatAmount(LedgerEntry e, Metadata metadata) {
-        String converted = String.format("%.02f", Math.abs(e.change()));
-
-        String[] parts = converted.split("\\.");
-        StringBuilder amount = new StringBuilder();
-        int count = 1;
-        for (int ind = parts[0].length() - 1; ind >= 0; ind--) {
-            if (((count % 3) == 0) && ind > 0) {
-                amount.insert(0, metadata.thSep + parts[0].charAt(ind));
-            } else {
-                amount.insert(0, parts[0].charAt(ind));
+            // Descritpion
+            String desc = e.description();
+            if (desc.length() > 25) {
+                desc = desc.substring(0, 22);
+                desc = desc + "...";
             }
-            count++;
-        }
 
-        amount = new StringBuilder(metadata.curSymb + amount + metadata.decSep + parts[1]);
+            // Change
+            DecimalFormat formatter = null;
 
-        if (e.change() < 0) {
-            amount.insert(0, "-");
-        }
-        return amount.toString();
+            if (loc.equals(NL_LOCALE)) { 
+                formatter = new DecimalFormat(curSymb + " #,##0.00 ;" + curSymb + " -#,##0.00 ");
+            } else {
+                formatter = new DecimalFormat(curSymb + "#,##0.00 ;(" + curSymb + "#,##0.00)");
+            } 
+
+            DecimalFormatSymbols symbols = formatter.getDecimalFormatSymbols();
+            symbols.setDecimalSeparator(decSep);
+            symbols.setGroupingSeparator(thSep);
+            formatter.setDecimalFormatSymbols(symbols);
+
+            String amount = formatter.format(e.change() / 100);
+
+            return String.format("%s | %-25s | %13s", date, desc, amount);
+
+        }).collect(Collectors.joining("\n"));
+
     }
 
-    private Metadata getMetadata (String loc, String cur) {
-        String curSymb;
-        if (cur.equals("USD")) {
-            curSymb = "$";
-        } else {
-            curSymb = "€";
-        }
-
-        switch (loc) {
-            case "en-US":
-                return new Metadata("Date       | Description               | Change       ",
-                        "MM/dd/yyyy", ".",  ",", curSymb);
-            case "nl-NL":
-                return new Metadata("Datum      | Omschrijving              | Verandering  ",
-                        "dd/MM/yyyy", ",", ".", curSymb);
-        }
-
-        throw new IllegalArgumentException("Invalid locale");
+    public record LedgerEntry (LocalDate localDate, String description, double change) {
     }
-
-    record Metadata (String header, String datPat, String decSep, String thSep, String curSymb){}
-
-    record LedgerEntry (LocalDate localDate, String description, double change) {}
 
 }

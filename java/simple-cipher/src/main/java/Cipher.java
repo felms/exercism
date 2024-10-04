af95c8b25e2f34a6f51011fd9c0ba7402a473c7f
Felipe <felipemoreira1@gmail.com>
2024-10-04 11:45:04 -0300

    Java - Simple Cipher

--- java/simple-cipher/src/main/java/Cipher.java
+++ java/simple-cipher/src/main/java/Cipher.java
@@ -1,15 +1,19 @@
-import java.util.Random;
-
 public class Cipher {
 
+    private static final int ALPHABET_LENGTH = 26;
     private static final char FIRST_LETTER = 'a';
     private static final char LAST_LETTER = 'z';
-    private static final int ALPHABET_LENGTH = 26;
 
     private final String key;
 
     public Cipher() {
-        this(generateRandomKey());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 100; i++) {
+            sb.append((char) (((int) Math.random() * ALPHABET_LENGTH) + FIRST_LETTER));
+        }
+
+        this.key = sb.toString();
     }
 
     public Cipher(String key) {
@@ -20,43 +24,33 @@ public class Cipher {
         return this.key;
     }
 
-    public String encode(String textToEncode) {
+    public String encode(String plainText) {
+
+        int keyLength = this.key.length();
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < textToEncode.length(); i++) {
-            int shift = this.key.charAt(i % key.length()) - FIRST_LETTER;
-            char c = (char) (textToEncode.charAt(i) + shift);
-            c = c > LAST_LETTER ? (char) (c - ALPHABET_LENGTH) : c;
-            sb.append(c);
+        for (int i = 0; i < plainText.length(); i++) {
+            int shift = this.key.charAt(i % keyLength) - FIRST_LETTER;
+            int pos = (plainText.charAt(i) + shift);
+            pos = pos > LAST_LETTER ? (pos - ALPHABET_LENGTH) : pos;
+            sb.append((char) pos);
         }
 
         return sb.toString();
     }
 
-    public String decode(String textToDecode) {
+    public String decode(String cipherText) {
 
+        int keyLength = this.key.length();
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < textToDecode.length(); i++) {
-            int shift = this.key.charAt(i) - FIRST_LETTER; 
-            char c = (char) (textToDecode.charAt(i) - shift);
-            c = c < FIRST_LETTER ? (char) (c + ALPHABET_LENGTH) : c;
-            sb.append(c);
+        for (int i = 0; i < cipherText.length(); i++) {
+            int shift = this.key.charAt(i % keyLength) - FIRST_LETTER;
+            int pos = ((cipherText.charAt(i) - shift));
+            pos = pos < FIRST_LETTER ? (pos + ALPHABET_LENGTH) : pos;
+            sb.append((char) pos);
         }
 
-        return sb.toString();
-    }
-
-    private static String generateRandomKey() {
-        StringBuilder sb = new StringBuilder();
-
-        Random random = new Random();
-
-        for (int i = 0; i < 100; i++) {
-            int pos = random.nextInt(ALPHABET_LENGTH);
-            sb.append((char) (FIRST_LETTER + pos));
-        }
-        System.out.println(sb);
         return sb.toString();
     }
 }

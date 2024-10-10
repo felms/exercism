3deaafe655751332eb27c35f6fa61e977aa9c168
Felipe <felipemoreira1@gmail.com>
2024-10-10 10:54:45 -0300

    Java - Word Count

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -1,33 +1,12 @@
+import java.util.AbstractMap;
 import java.util.Map;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+import java.util.stream.Collectors;
 
-public class WordCount{
-
-   
+class WordCount {
     public Map<String, Integer> phrase(String input) {
-        Map<String, Integer> wordCount = new HashMap<>();
-
-        List<String> words = Arrays.asList(getWords(input));
-        words.forEach(word -> wordCount.putIfAbsent(word, Collections.frequency(words, word)));
-
-        return wordCount;
+        return new Scanner(input.toLowerCase()).findAll("\\w+('\\w+)?")
+                .collect(Collectors.groupingBy(MatchResult::group, Collectors.summingInt(e -> 1)));
     }
-
-    private String[] getWords(String input) {
-        
-        String[] w = input.toLowerCase().replaceAll("[,:;]", " ").trim().split("\\s+");
-
-        for (int i = 0; i < w.length; i++) {
-            w[i] = w[i].replaceAll("^['\"]|[\\.\\?!:]|[^a-z0-9']", "");
-            w[i] = w[i].replaceAll("['\"]$", "");
-            w[i] = w[i].replaceAll("['\"]$", "");
-        }
-        
-        return w;
-    }
-
 }
-

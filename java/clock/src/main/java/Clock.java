5bdb68ab7b999eea1517fb9de6ce183b99d8fdd5
Felipe <felipemoreira1@gmail.com>
2024-10-15 16:17:09 -0300

    Java - Clock

--- java/clock/src/main/java/Clock.java
+++ java/clock/src/main/java/Clock.java
@@ -1,62 +1,31 @@
-import java.util.Objects;
+class Clock {
 
-public class Clock {
-
-    private final int DAY_IN_MINUTES = 1440;
+    private final static int ONE_HOUR_MINUTES = 60;
+    private final static int ONE_DAY_IN_MINUTES = 1440;
 
     private int minutes;
 
-    public Clock(int hours, int minutes) {
-
-        int h = hours >= 0 ? hours % 24 : 24 + hours % 24;
-
-        int m;
-        if (minutes >= 60) {
-            h += minutes / 60;
-            h %= 24;
-            m = minutes % 60;
-        } else if (minutes >= 0) {
-            m = minutes;
-        } else {
-            m = -Math.abs(minutes) % 60;
-            h += -(Math.abs(minutes) % DAY_IN_MINUTES) / 60;
-            h = h < 0 ? 24 + h : h;
-        }
-
-        this.minutes = h * 60;
-        this.minutes += m;
+    Clock(int hours, int minutes) {
+        this.minutes = 0;
+        add(hours * ONE_HOUR_MINUTES + minutes);
     }
 
-    public void add(int minutes) {
-        minutes = minutes < 0 ? -(Math.abs(minutes) % DAY_IN_MINUTES) : minutes;
-        this.minutes += minutes;
-        this.minutes = this.minutes < 0 ? this.minutes + DAY_IN_MINUTES : this.minutes % DAY_IN_MINUTES;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-
-        if (o == null) {
-            return false;
-        }
-
-        Clock clock = (Clock) o;
-
-        return minutes == clock.minutes;
-    }
-
-    @Override
-    public int hashCode() {
-        return this.minutes * 37;
+    void add(int minutes) {
+        int m = (this.minutes + minutes) % ONE_DAY_IN_MINUTES;
+        this.minutes = m >= 0 ? m : m + ONE_DAY_IN_MINUTES;
     }
 
     @Override
     public String toString() {
-        int h = this.minutes / 60;
-        int m = this.minutes % 60;
-        return String.format("%02d:%02d", h, m);
+        return String.format("%02d:%02d", this.minutes / ONE_HOUR_MINUTES, this.minutes % ONE_HOUR_MINUTES);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+
+        return this.toString().equals(obj.toString());
     }
 }

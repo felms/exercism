48373bfe137aba89f8455e2181900772305e63ae
Felipe <felipemoreira1@gmail.com>
2024-10-18 09:12:47 -0300

    Java - State Of Tic Tac Toe

--- java/state-of-tic-tac-toe/src/main/java/StateOfTicTacToe.java
+++ java/state-of-tic-tac-toe/src/main/java/StateOfTicTacToe.java
@@ -1,86 +1,57 @@
+import java.util.List;
+
 class StateOfTicTacToe {
+
+    private static final char X_PLAYER = 'X';
+    private static final char O_PLAYER = 'O';
+
     public GameState determineState(String[] board) {
+        String theBoard = String.join("", board);
+        int numberOfXs = theBoard.replaceAll("[^X]", "").length();
+        int numberOfOs = theBoard.replaceAll("[^O]", "").length();
 
-        checkBoard(board);
+        if ((numberOfXs - numberOfOs) > 1) {
+            throw new IllegalArgumentException("Wrong turn order: X went twice");
+        }
 
-        if (playerWon("X", board) || playerWon("O", board)) {
+        if (numberOfOs > numberOfXs) {
+            throw new IllegalArgumentException("Wrong turn order: O started");
+        }
+
+        boolean xWon = playerWon(X_PLAYER, theBoard);
+        boolean oWon = playerWon(O_PLAYER, theBoard);
+
+        if (xWon && (numberOfXs == numberOfOs)) {
+            throw new IllegalArgumentException("Impossible board: game should have ended after the game was won");
+        }
+
+        if (xWon && oWon) {
+            throw new IllegalArgumentException("Impossible board: game should have ended after the game was won");
+        }
+
+        if (xWon || oWon) {
             return GameState.WIN;
         }
 
-        if (gameTied(board)) {
+        if (!theBoard.contains(" ")) {
             return GameState.DRAW;
         }
 
         return GameState.ONGOING;
     }
 
-    boolean playerWon(String player, String[] board) {
+    private boolean playerWon(char player, String board) {
 
-        return horizontalWin(player, board)
-                || verticalWin(player, board)
-                || diagonalWin(player, board);
+        List<List<Integer>> winningPositions = List.of(
+                List.of(0, 1, 2), List.of(3, 4, 5), List.of(6, 7, 8), // horizontal
+                List.of(0, 3, 6), List.of(1, 4, 7), List.of(2, 5, 8), // vertical
+                List.of(0, 4, 8), List.of(2, 4, 6)                    // diagonal
+        );
+
+        return winningPositions.stream().anyMatch(pos -> wonAtPos(player, board, pos));
     }
 
-    boolean gameTied(String[] board) {
-        String completeBoard = board[0] + board[1] + board[2];
-
-        return !completeBoard.contains(" ");
-    }
-    boolean horizontalWin(String player, String[] board) {
-        String playerRow = player + player + player;
-
-        return playerRow.equals(board[0])
-                || playerRow.equals(board[1])
-                || playerRow.equals(board[2]);
-    }
-
-    boolean verticalWin(String player, String[] board) {
-        String playerRow = player + player + player;
-
-        String firstColumn = "" + board[0].charAt(0) + board[1].charAt(0) + board[2].charAt(0);
-        String secondColumn = "" + board[0].charAt(1) + board[1].charAt(1) + board[2].charAt(1);
-        String thirdColumn = "" + board[0].charAt(2) + board[1].charAt(2) + board[2].charAt(2);
-
-        return playerRow.equals(firstColumn)
-                || playerRow.equals(secondColumn)
-                || playerRow.equals(thirdColumn);
-    }
-
-    boolean diagonalWin(String player, String[] board) {
-        String playerRow = player + player + player;
-
-        String firstDiagonal = "" + board[0].charAt(0) + board[1].charAt(1) + board[2].charAt(2);
-        String secondDiagonal = "" + board[0].charAt(2) + board[1].charAt(1) + board[2].charAt(0);
-
-        return playerRow.equals(firstDiagonal)
-                || playerRow.equals(secondDiagonal);
-    }
-
-    void checkBoard(String[] board) {
-
-        String completeBoard = board[0] + board[1] + board[2];
-
-        int countX = 0;
-        int countO = 0;
-
-        for (char c : completeBoard.toCharArray()) {
-            if (c == 'X') {
-                countX ++;
-            } else if (c == 'O') {
-                countO++;
-            }
-        }
-
-        if (countO > countX) {
-            throw new IllegalArgumentException("Wrong turn order: O started");
-        }
-
-        if (countX - countO > 1) {
-            throw new IllegalArgumentException("Wrong turn order: X went twice");
-        }
-
-        if (playerWon("X", board) && playerWon("O", board)) {
-            throw new IllegalArgumentException("Impossible board: game should have ended after the game was won");
-        }
+    private boolean wonAtPos(char player, String board, List<Integer> position) {
+        return position.stream().allMatch(i -> board.charAt(i) == player);
     }
 }

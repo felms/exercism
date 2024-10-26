fd1ea41caaa6d3f23a2539119486a43430be6437
felipe <felipemoreira1@gmail.com>
2024-10-26 19:26:53 -0300

    Java - Say

--- java/say/src/main/java/Say.java
+++ java/say/src/main/java/Say.java
@@ -6,140 +6,86 @@ public class Say {
             throw new IllegalArgumentException();
         }
 
-        String s = "";
         if (number == 0) {
             return "zero";
         }
 
-        s = oneToOneThousand(number);
+        int b = (int) (number / 1_000_000_000);
+        int m = (int) (number % 1_000_000_000) / 1_000_000;
+        int t = (int) (number % 1_000_000);
+        String res = billions(b);
+        res += m > 0 ? " " + millions(m) : "";
+        res += t > 0 ? " " + thousands(t) : "";
 
-        long th = number % 1_000_000L;
-        th /= 1000;
-        if (th > 0) {
-            s = oneToOneThousand(th) + " thousand " + s;
-        }
-
-        long m = number % 1_000_000_000L;
-        m /= 1_000_000;
-        if (m > 0) {
-            s = oneToOneThousand(m) + " million " + s;
-        }
-
-        long b = number % 1_000_000_000_000L;
-        b /= 1_000_000_000;
-        if (b > 0) {
-            s = oneToOneThousand(b) + " billion " + s;
-        }
-
-        return s.trim();
+        return res.trim();
     }
 
-    private String oneToOneThousand(long number) {        
-
-        String s = "";
-
-        int u = (int) (number % 10);
-        s += units(u);
-
-        int t = (int) (number % 100);
-
-        if (t > 10 && t < 20) {
-            s = teens(t);
-        } else if (t >= 20) {
-            t -= u;
-            if (u == 0) {
-                s = tens(t);
-            } else {
-                s = tens(t) + "-" + s;
-            }
-        }
-
-        int h = (int) (number % 1000);
-        h /= 100;
-        if (h > 0) {
-            s = units(h) + " hundred " + s;
-        }
-
-        return s.trim();
+    private String billions(int number) {
+        return number > 0 ? hundreds(number) + " billion" : "";
     }
 
-    private String units(int number) {
-        switch (number) {
-            case 1:
-                return "one";
-            case 2:
-                return "two";
-            case 3:
-                return "three";
-            case 4:
-                return "four";
-            case 5:
-                return "five";
-            case 6:
-                return "six";
-            case 7:
-                return "seven";
-            case 8:
-                return "eight";
-            case 9:
-                return "nine";
-            default:
-                return "";
-                
-        }
+    private String millions(int number) {
+        return number > 0 ? hundreds(number) + " million" : "";
     }
 
-    private String teens(int number) {
-        
-        switch (number) {
-            case 11:
-                return "eleven";
-            case 12:
-                return "twelve";
-            case 13:
-                return "thirteen";
-            case 14:
-                return "fourteen";
-            case 15:
-                return "fifteen";
-            case 16:
-                return "sixteen";
-            case 17:
-                return "seventeen";
-            case 18:
-                return "eighteen";
-            case 19:
-                return "nineteen";
-                
-        }
+    private String thousands(int number) {
+        int tn = number / 1000;
+        return (tn > 0 ? hundreds(tn) + " thousand" : "")
+                + ((tn > 0 && number % 1000 > 0) ? " " : "")
+                + ((number % 1000 > 0) ? hundreds(number % 1000) : "");
+    }
 
-        return "";
+    private String hundreds(int number) {
+        int n = number / 100;
+        String t = number % 100 != 0 ? " " + tens(number % 100) : "";
+        return n == 0 ? tens(number) : units(n) + " hundred" + t;
     }
 
     private String tens(int number) {
-        
-        switch (number) {
-            case 10:
-                return "ten";
-            case 20:
-                return "twenty";
-            case 30:
-                return "thirty";
-            case 40:
-                return "forty";
-            case 50:
-                return "fifty";
-            case 60:
-                return "sixty";
-            case 70:
-                return "seventy";
-            case 80:
-                return "eighty";
-            case 90:
-                return "ninety";
-            default:
-                return "";
-                
-        }
+        int n = number / 10;
+        String u = number % 10 != 0 ? "-" + units(number % 10) : "";
+        return switch (n) {
+            case 1 -> teens(number);
+            case 2 -> "twenty" + u;
+            case 3 -> "thirty" + u;
+            case 4 -> "forty" + u;
+            case 5 -> "fifty" + u;
+            case 6 -> "sixty" + u;
+            case 7 -> "seventy" + u;
+            case 8 -> "eighty" + u;
+            case 9 -> "ninety" + u;
+            default -> units(number);
+        };
+    }
+
+    private String teens(int number) {
+        return switch (number) {
+            case 10 -> "ten";
+            case 11 -> "eleven";
+            case 12 -> "twelve";
+            case 13 -> "thirteen";
+            case 14 -> "fourteen";
+            case 15 -> "fifteen";
+            case 16 -> "sixteen";
+            case 17 -> "seventeen";
+            case 18 -> "eighteen";
+            case 19 -> "nineteen";
+            default -> "";
+        };
+    }
+
+    private String units(int number) {
+        return switch (number) {
+            case 1 -> "one";
+            case 2 -> "two";
+            case 3 -> "three";
+            case 4 -> "four";
+            case 5 -> "five";
+            case 6 -> "six";
+            case 7 -> "seven";
+            case 8 -> "eight";
+            case 9 -> "nine";
+            default -> "";
+        };
     }
 }

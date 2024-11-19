a0c24df3185fdd4bd14307fe5b13431b8df61dbe
Felipe <felipemoreira1@gmail.com>
2024-11-19 17:33:45 -0300

    JavaScript - Run-Length Encoding

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -1,55 +1,8 @@
-export const encode = (inputString) => {
-  if (inputString === '') {
-    return '';
-  }
-  let sb = '';
+export const encode = (inputString) => 
+    inputString.replaceAll(/(.)\1+/g, 
+        (match, str, ...rem) => `${match.length}${str}`);
 
-  inputString += '*'; // TODO retirar essa gambiarra
-  let letters = inputString.split(''); 
-  let previousLetter = letters[0];
-  let letterCount = 1;
-  for (let i = 1; i < letters.length; i++) {
-    if (letters[i] === previousLetter) {
-      letterCount++;
-    } else if (letterCount > 1){
-      sb = sb.concat('' + letterCount + previousLetter);
-      letterCount = 1;
-    } else if (letterCount === 1) {
-      sb = sb.concat(previousLetter);
-      letterCount = 1;
-    }
-    previousLetter = letters[i];
-  }
+export const decode = (inputString) => 
+    inputString.replaceAll(/(\d+)([ a-z])/gi, 
+        (_, length, str, ...rem) => str.repeat(length));
 
-  return sb.toString();
-};
-
-export const decode = (inputString) => {
-  if (inputString === '') {
-    return '';
-  }
-
-  let sb = '';
-  let letters = inputString.split('');
-
-  while (letters.length > 0) {
-    let letter = letters.shift();
-
-    if (/\d/.test(letter)) {
-      let number = '';
-      number = number.concat(letter);
-      while (/\d/.test(letters[0])) {
-        number = number.concat(letters.shift());
-      }
-      letter = letters.shift();
-      let n = parseInt(number);
-      for (let i = 0; i < n; i++) {
-        sb = sb.concat(letter);
-      }
-    } else {
-      sb = sb.concat(letter);
-    }
-  }				
-
-  return sb;
-};

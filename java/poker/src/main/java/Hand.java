396329f48de1358a66989d675feca46a45c344b7
felipe <felipemoreira1@gmail.com>
2024-11-23 20:16:11 -0300

    Java - Poker (WIP)

--- java/poker/src/main/java/Hand.java
+++ java/poker/src/main/java/Hand.java
@@ -1,116 +1,99 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public  class Hand implements Comparable<Hand>{
 
-    private final List<Card> hand;
-    private final HandRankingCategorie categorie;
+    private final List<Card> cards;
+    private final HandCategory handCategory;
 
-    public Hand(String inputHand) {
-        String[] cards = inputHand.split("\\s+");
-        hand = new ArrayList<>();
-        Arrays.stream(cards).forEach(card -> hand.add(new Card(card)));
-        this.categorie = categorizeHand(this.hand);
+    public Hand(String handString) {
+        this.cards = Arrays.stream(handString.split("\\s+")).map(Card::new).toList();
+        this.handCategory = Hand.categorizeHand(this.cards);
     }
-
 
     public List<Card> getCards() {
-        return new ArrayList<>(this.hand);
+        return this.cards.stream().sorted(Comparator.reverseOrder()).toList();
     }
 
-    public HandRankingCategorie getCategorie() {
-        return this.categorie;
-    }
- 
-    private HandRankingCategorie categorizeHand(List<Card> hand) {
-        Map<Suit, Integer> suitFrequency = new HashMap<>();
-        Arrays.stream(Suit.values()).forEach(suit -> suitFrequency.put(suit, 0));
-
-        Map<Hank, Integer> hankFrequency = new HashMap<>();
-        Arrays.stream(Hank.values()).forEach(hank -> hankFrequency.put(hank, 0));
-
-        hand.forEach(card -> {
-            Hank h = card.getHank();
-            Suit s = card.getSuit();
-            int f = hankFrequency.get(h);
-            hankFrequency.put(h, f + 1);
-            f = suitFrequency.get(s);
-            suitFrequency.put(s, f + 1);
-        });
-
-        if (suitFrequency.entrySet().stream().filter(entry -> entry.getValue() != 0).distinct().count() == 1) {
-            if (isStraight(hand)) {
-                return HandRankingCategorie.STRAIGHT_FLUSH;
-            }
-
-            return HandRankingCategorie.FLUSH;
-        }
-
-        if (hankFrequency.values().stream().filter(f -> f == 4).count() > 0) {
-            return HandRankingCategorie.FOUR_OF_A_KIND;
-        }
-
-        if (hankFrequency.entrySet().stream().filter(entry -> entry.getValue() != 0).distinct().count() == 2) {
-            return HandRankingCategorie.FULL_HOUSE;
-        }
-
-        if (isStraight(hand)) {
-            return HandRankingCategorie.STRAIGHT;
-        }
-
-        if (hankFrequency.values().stream().filter(f -> f == 3).count() == 1) {
-            return HandRankingCategorie.THREE_OF_A_KIND;
-        }
-
-        if (hankFrequency.values().stream().filter(f -> f == 2).count() == 2) {
-            return HandRankingCategorie.TWO_PAIR;
-        }
-
-        if (hankFrequency.values().stream().filter(f -> f == 2).count() == 1) {
-            return HandRankingCategorie.ONE_PAIR;
-        }
-
-        return HandRankingCategorie.HIGH_CARD;
+    HandCategory getHandCategory() {
+        return this.handCategory;
     }
 
-    private boolean isStraight(List<Card> list) {
-        List<Hank> hankList = new ArrayList<>();
-        list.forEach(card -> hankList.add(card.getHank()));
-        Collections.sort(hankList);
-        List<Hank> values = Arrays.asList(Hank.values());
-        int i0 = values.indexOf(hankList.get(0));
-        int i1 = values.indexOf(hankList.get(1));
-        int i2 = values.indexOf(hankList.get(2));
-        int i3 = values.indexOf(hankList.get(3));
-        int i4 = values.indexOf(hankList.get(4));
+    static HandCategory categorizeHand(List<Card> cards) {
 
-        if ((i1 == i0 + 1 && i2 == i1 + 1 && i3 == i2 + 1 && i4 == i3 + 1) ||
-                (hankList.get(0).equals(Hank.TWO) 
-                && hankList.get(1).equals(Hank.THREE)
-                && hankList.get(2).equals(Hank.FOUR)
-                && hankList.get(3).equals(Hank.FIVE)
-                && hankList.get(4).equals(Hank.ACE))) {
-            return true;
+        List<Integer> sortedRanks = cards.stream().map(Card::getHankValue).sorted(Comparator.reverseOrder()).toList();
+
+        boolean isStraight = sortedRanks.getFirst() - sortedRanks.getLast() == 4
+                || (sortedRanks.getFirst() == 14 && (sortedRanks.get(1) - sortedRanks.getLast() == 3));
+
+        boolean isFlush = cards.stream().map(Card::getSuit).distinct().count() == 1;
+
+        String freq = cards.stream().map(Card::getHankValue)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .values().stream().sorted(Comparator.reverseOrder())
+                .map(Object::toString).collect(Collectors.joining());
+
+        if (isStraight && isFlush) {
+            return HandCategory.STRAIGHT_FLUSH;
         }
 
-        return false;
-    }
+        if (freq.equals("41")) {
+            return HandCategory.FOUR_OF_A_KIND;
+        }
 
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
+        if (freq.equals("32")) {
+            return HandCategory.FULL_HOUSE;
+        }
 
-        hand.forEach(card -> sb.append(card.toString()).append(" "));
+        if (isFlush) {
+            return HandCategory.FLUSH;
+        }
 
-        return sb.toString().trim();
+        if (isStraight) {
+            return HandCategory.STRAIGHT;
+        }
+
+        if (freq.equals("311")) {
+            return HandCategory.THREE_OF_A_KIND;
+        }
+
+        if (freq.equals("221")) {
+            return HandCategory.TWO_PAIR;
+        }
+
+        if (freq.equals("2111")) {
+            return HandCategory.ONE_PAIR;
+        }
+
+        return HandCategory.HIGH_CARD;
     }
 
     @Override
     public int compareTo(Hand o) {
-        
-        return this.categorie.compareTo(o.getCategorie());
+        return this.handCategory.getValue() - o.handCategory.getValue();
+    }
+
+    @Override
+    public String toString() {
+        return this.cards.stream().map(Card::toString).collect(Collectors.joining(" "));
+    }
+}
+
+enum HandCategory {
+
+    HIGH_CARD(0), ONE_PAIR(1), TWO_PAIR(2), THREE_OF_A_KIND(3), STRAIGHT(4),
+    FLUSH(5), FULL_HOUSE(6), FOUR_OF_A_KIND(7), STRAIGHT_FLUSH(8);
+
+    private final int value;
+
+    HandCategory(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
     }
 }

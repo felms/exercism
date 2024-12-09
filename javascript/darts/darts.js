e21e5a6a541db478dd1cb9170a900274fee71d81
Felipe <felipemoreira1@gmail.com>
2024-12-09 13:24:40 -0300

    JavaScript - Darts

--- javascript/darts/darts.js
+++ javascript/darts/darts.js
@@ -1,11 +1,17 @@
 export const score = (x, y) => {
-  let distance = distanceBetweenPoints(0, 0, x, y);
-  return distance <= 1 ? 10 
-    : distance <= 5 ? 5
-    : distance <= 10 ? 1
-    : 0;
-};
+    let d = Math.sqrt((x ** 2) + (y ** 2));
 
-const distanceBetweenPoints = (x1, y1, x2, y2) => {
-  return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    if (d <= 1) {
+        return 10;
+    }
+
+    if (d <= 5) {
+        return 5;
+    }
+
+    if (d <= 10) {
+        return 1;
+    }
+
+    return 0;
 };

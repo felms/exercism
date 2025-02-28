496f92cc98b194e7f8ca0e25a5b0a58cfbe85528
Felipe <felipemoreira1@gmail.com>
2025-02-28 11:23:30 -0300

    Java - Rational Numbers

--- java/rational-numbers/src/main/java/Rational.java
+++ java/rational-numbers/src/main/java/Rational.java
@@ -1,28 +1,14 @@
+import java.util.Objects;
+
 class Rational {
 
     private final int numerator;
     private final int denominator;
 
     Rational(int numerator, int denominator) {
-        int num = numerator;
-
-        if (denominator == 0) {
-            throw new IllegalArgumentException("The denominator can't be zero");
-        }
-
-        int den = denominator;
-
-        if (den < 0 && num > 0) {
-            num *= -1;
-            den *= -1;
-        }
-
-        
-
-        int gcd = gcd(num, den);
-
-        this.numerator = num / gcd;
-        this.denominator = den / gcd;
+        int gcd = gcd(numerator, denominator);
+        this.numerator = numerator / gcd;
+        this.denominator = denominator / gcd;
     }
 
     int getNumerator() {
@@ -33,91 +19,58 @@ class Rational {
         return this.denominator;
     }
 
-    public Rational add(Rational other) {
-
-        int a1 = this.getNumerator();
-        int b1 = this.getDenominator();
-        int a2 = other.getNumerator();
-        int b2 = other.getDenominator();
-
-        int newNumerator = a1 * b2 + a2 * b1;
-        int newDenominator = b1 * b2;
-
-        return new Rational(newNumerator, newDenominator);
+    Rational add(Rational other) {
+        return new Rational(
+                this.getNumerator() * other.getDenominator() + other.getNumerator() * this.getDenominator(),
+                this.getDenominator() * other.getDenominator());
     }
 
-    public Rational subtract(Rational other) {
-        int a1 = this.getNumerator();
-        int b1 = this.getDenominator();
-        int a2 = other.getNumerator();
-        int b2 = other.getDenominator();
-
-        int newNumerator = a1 * b2 - a2 * b1;
-        int newDenominator = b1 * b2;
-
-        return new Rational(newNumerator, newDenominator);
+    Rational subtract(Rational other) {
+        return new Rational(
+                this.getNumerator() * other.getDenominator() - other.getNumerator() * this.getDenominator(),
+                this.getDenominator() * other.getDenominator());
     }
 
-    public Rational multiply(Rational other) {
-        int a1 = this.getNumerator();
-        int b1 = this.getDenominator();
-        int a2 = other.getNumerator();
-        int b2 = other.getDenominator();
-
-        int newNumerator = a1 * a2;
-        int newDenominator = b1 * b2;
-
-        return new Rational(newNumerator, newDenominator);
+    Rational multiply(Rational other) {
+        return new Rational(
+                this.getNumerator() * other.getNumerator(),
+                this.getDenominator() * other.getDenominator());
     }
 
-    public Rational divide(Rational other) {
-        int a1 = this.getNumerator();
-        int b1 = this.getDenominator();
-        int a2 = other.getNumerator();
-        int b2 = other.getDenominator();
+    Rational divide(Rational other) {
+        return new Rational(
+                this.getNumerator() * other.getDenominator(),
+                other.getNumerator() * this.getDenominator());
+    }
 
-        if (a2 == 0) {
-            throw new IllegalArgumentException("The numerator of the second rational can't be zero. (Can't divide by zero)");
+    Rational abs() {
+        return new Rational(Math.abs(this.getNumerator()), Math.abs(this.getDenominator()));
+    }
+
+    Rational pow(int power) {
+        if (power > 0) {
+            return new Rational(
+                    (int) Math.pow(this.getNumerator(), power),
+                    (int) Math.pow(this.getDenominator(), power));
         }
 
-        if (a1 < 0 && a2 < 0) {
-            a1 *= -1;
-            a2 *= -1;
-        }
-
-        int newNumerator = a1 * b2;
-        int newDenominator = a2 * b1;
-
-        return new Rational(newNumerator, newDenominator);
+        int m = Math.abs(power);
+        return new Rational(
+                (int) Math.pow(this.getDenominator(), m),
+                (int) Math.pow(this.getNumerator(), m));
     }
 
-    public Rational abs() {
-        int newNumerator = Math.abs(this.getNumerator());
-        int newDenominator = Math.abs(this.getDenominator());
-
-        return new Rational(newNumerator, newDenominator);
-    }
-
-    public Rational pow(int exponent) {
-        int m = Math.abs(exponent);
-        int newNumerator = (int) Math.pow(this.getNumerator(), m);
-        int newDenominator = (int) Math.pow(this.getDenominator(), m);
-        return new Rational(newNumerator, newDenominator);
-    }
-
-    public double exp(double exponent) {
-
-        return Math.pow(Math.pow(exponent, 1.0 / this.getDenominator()), this.getNumerator());
+    double exp(double exponent) {
+        return Math.pow(Math.pow(exponent, this.getNumerator()), 1.0 / this.getDenominator());
     }
 
     private int gcd(int a, int b) {
         if (b == 0) {
-            return Math.abs(a);
+            return a;
         }
 
         return gcd(b, a % b);
     }
-
 
     @Override
     public String toString() {
@@ -126,22 +79,16 @@ class Rational {
 
     @Override
     public boolean equals(Object obj) {
-        if (obj == null || !this.getClass().isAssignableFrom(obj.getClass())) {
-            return false;
+        if (obj instanceof Rational other) {
+            return this.getNumerator() == other.getNumerator()
+                    && this.getDenominator() == other.getDenominator();
         }
 
-        Rational other = (Rational) obj;
-        return this.getNumerator() == other.getNumerator()
-            && this.getDenominator() == other.getDenominator();
+        return false;
     }
 
     @Override
     public int hashCode() {
-        int prime = 31;
-        int result = 1;
-        result = prime * result + this.getNumerator();
-        result = prime * result + this.getDenominator();
-
-        return result;
+        return Objects.hash(this.getNumerator(), this.getDenominator());
     }
 }
